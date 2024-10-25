63550b3adeaeeb0954a6b313dd5e19f1df1b8ee2
jujcgu <juanjosecastrogutierrez@gmail.com>
2024-10-25 13:52:01 -0500

    removed unused imports from KardexItemModelAssembler

--- src/main/java/com/coagronet/kardexItem/asemblers/KardexItemModelAssembler.java
+++ src/main/java/com/coagronet/kardexItem/asemblers/KardexItemModelAssembler.java
@@ -1,15 +1,15 @@
 package com.coagronet.kardexItem.asemblers;
 
-import com.coagronet.kardex.controllers.KardexController;
-import com.coagronet.kardexItem.KardexItem;
-import com.coagronet.kardexItem.controllers.KardexItemController;
-import com.coagronet.kardexItem.dtos.KardexItemDTO;
-import com.coagronet.kardexItem.mappers.KardexItemMapper;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
+
+import com.coagronet.kardexItem.KardexItem;
+import com.coagronet.kardexItem.controllers.KardexItemController;
+import com.coagronet.kardexItem.dtos.KardexItemDTO;
+import com.coagronet.kardexItem.mappers.KardexItemMapper;
 
 @Component
 public class KardexItemModelAssembler implements RepresentationModelAssembler<KardexItem, EntityModel<KardexItemDTO>> {
@@ -24,8 +24,12 @@ public class KardexItemModelAssembler implements RepresentationModelAssembler<Ka
     public EntityModel<KardexItemDTO> toModel(KardexItem kardexItem) {
         KardexItemDTO kardexItemDTO = kardexItemMapper.toDto(kardexItem);
         return EntityModel.of(kardexItemDTO,
-                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(KardexItemController.class).findKardexItem(kardexItem.getId())).withSelfRel(),
-                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(KardexItemController.class).all(2, Pageable.unpaged())).withRel("kardexItem"));
+                WebMvcLinkBuilder.linkTo(
+                        WebMvcLinkBuilder.methodOn(KardexItemController.class).findKardexItem(kardexItem.getId()))
+                        .withSelfRel(),
+                WebMvcLinkBuilder
+                        .linkTo(WebMvcLinkBuilder.methodOn(KardexItemController.class).all(2, Pageable.unpaged()))
+                        .withRel("kardexItem"));
     }
 
 }
