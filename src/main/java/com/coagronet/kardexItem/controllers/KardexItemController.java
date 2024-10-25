6df1e28f84630c3a12327c182e28be2df18bc30d
jujcgu <juanjosecastrogutierrez@gmail.com>
2024-10-25 13:48:06 -0500

    fixed put request in KardexItemController

--- src/main/java/com/coagronet/kardexItem/controllers/KardexItemController.java
+++ src/main/java/com/coagronet/kardexItem/controllers/KardexItemController.java
@@ -1,11 +1,7 @@
 package com.coagronet.kardexItem.controllers;
 
-import com.coagronet.exceptionHandler.ResourceNotFoundException;
-import com.coagronet.kardexItem.KardexItem;
-import com.coagronet.kardexItem.asemblers.KardexItemModelAssembler;
-import com.coagronet.kardexItem.dtos.KardexItemDTO;
-import com.coagronet.kardexItem.mappers.KardexItemMapper;
-import com.coagronet.kardexItem.repositories.KardexItemRepository;
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,10 +11,23 @@ import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
+import com.coagronet.exceptionHandler.ResourceNotFoundException;
+import com.coagronet.kardexItem.KardexItem;
+import com.coagronet.kardexItem.asemblers.KardexItemModelAssembler;
+import com.coagronet.kardexItem.dtos.KardexItemDTO;
+import com.coagronet.kardexItem.mappers.KardexItemMapper;
+import com.coagronet.kardexItem.repositories.KardexItemRepository;
 
 @RestController
 @RequestMapping("/api/v1/kardexItem")
@@ -69,14 +78,21 @@ public class KardexItemController {
     }
 
     @PutMapping("/{requestedId}")
-    private ResponseEntity<EntityModel<KardexItemDTO>> updateKardexItem(@PathVariable Integer requestedId, @RequestBody KardexItemDTO KardexItemDTOUpdate) {
-        KardexItem kardexItem = kardexItemMapper.INSTANCE.toEntity(KardexItemDTOUpdate);
-        kardexItem.setId(requestedId);
-        if (null != kardexItem) {
-            kardexItemRepository.save(kardexItem);
-            return ResponseEntity.noContent().build();
+    public ResponseEntity<EntityModel<KardexItemDTO>> updateKardexItem(
+            @PathVariable Integer requestedId,
+            @RequestBody KardexItemDTO kardexItemDTOUpdate) {
+
+        // Verifica si el KardexItem existe
+        if (!kardexItemRepository.existsById(requestedId)) {
+            return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.notFound().build();
+
+        // Mapea y actualiza
+        KardexItem kardexItem = KardexItemMapper.INSTANCE.toEntity(kardexItemDTOUpdate);
+        kardexItem.setId(requestedId);
+        kardexItemRepository.save(kardexItem);
+
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
