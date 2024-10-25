88796105cb777c814b22da3785f44cce9429417f
jujcgu <juanjosecastrogutierrez@gmail.com>
2024-10-25 14:05:29 -0500

    removed unused imports from GrupoRepository

--- src/main/java/com/coagronet/grupo/repositories/GrupoRepository.java
+++ src/main/java/com/coagronet/grupo/repositories/GrupoRepository.java
@@ -1,16 +1,15 @@
 package com.coagronet.grupo.repositories;
 
-import com.coagronet.empresa.Empresa;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.coagronet.empresa.Empresa;
 import com.coagronet.grupo.Grupo;
-
-import java.util.List;
 
 @Repository
 public interface GrupoRepository extends JpaRepository<Grupo, Long> {
