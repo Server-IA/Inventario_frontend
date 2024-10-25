a344ef868620ef92b71df49e850106ac225f8361
jujcgu <juanjosecastrogutierrez@gmail.com>
2024-10-25 13:42:18 -0500

    removed unused imports from TipoMovimientoRepository

--- src/main/java/com/coagronet/tipoMovimiento/reposritories/TipoMovimientoRepository.java
+++ src/main/java/com/coagronet/tipoMovimiento/reposritories/TipoMovimientoRepository.java
@@ -1,11 +1,11 @@
 package com.coagronet.tipoMovimiento.reposritories;
 
-import com.coagronet.empresa.Empresa;
-import com.coagronet.tipoMovimiento.TipoMovimiento;
+import java.util.List;
+
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import com.coagronet.tipoMovimiento.TipoMovimiento;
 
 public interface TipoMovimientoRepository extends JpaRepository<TipoMovimiento, Integer> {
     List<TipoMovimiento> findByEstadoNot(Integer estado, Sort sort);
