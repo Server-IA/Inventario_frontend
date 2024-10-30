2e11ed65224c4f0b8cdde5cc8804b77b30aa9c3e
jujcgu <juanjosecastrogutierrez@gmail.com>
2024-10-30 15:55:01 -0500

    changed DatosListadoProduccion to DatosListadoCortoProduccion

--- src/main/java/com/coagronet/produccion/dtos/DatosListadoProduccion.java
+++
@@ -1,9 +0,0 @@
-package com.coagronet.produccion.dtos;
-
-import com.coagronet.produccion.Produccion;
-
-public record DatosListadoProduccion(Integer id, String nombre) {
-    public DatosListadoProduccion(Produccion produccion) {
-        this(produccion.getId(), produccion.getNombre());
-    }
-}
