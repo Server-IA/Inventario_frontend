57448f19a6e7badc553e02914ffec561fc6b5f6a
jujcgu <juanjosecastrogutierrez@gmail.com>
2024-12-13 15:12:09 -0500

    add method existsByIdAndAlmacenSedeEmpresaIdAndEstadoIdNot

--- src/main/java/com/coagronet/pedido/repositories/PedidoRepository.java
+++ src/main/java/com/coagronet/pedido/repositories/PedidoRepository.java
@@ -10,18 +10,23 @@ import com.coagronet.pedido.Pedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
-    Optional<Pedido> findByIdAndAlmacenSedeEmpresaId(
-            Integer id,
-            Long empresaId);
+        Optional<Pedido> findByIdAndAlmacenSedeEmpresaId(
+                        Integer id,
+                        Long empresaId);
 
-    Page<Pedido> findByAlmacenIdAndAlmacenSedeEmpresaIdAndEstadoIdNot(
-            Integer almacenId,
-            Long empresaId,
-            Integer estado,
-            Pageable pageable);
+        Page<Pedido> findByAlmacenIdAndAlmacenSedeEmpresaIdAndEstadoIdNot(
+                        Integer almacenId,
+                        Long empresaId,
+                        Integer estado,
+                        Pageable pageable);
 
-    boolean existsByIdAndAlmacenSedeEmpresaId(
-            Integer id,
-            Long empresaId);
+        boolean existsByIdAndAlmacenSedeEmpresaId(
+                        Integer id,
+                        Long empresaId);
+
+        boolean existsByIdAndAlmacenSedeEmpresaIdAndEstadoIdNot(
+                        Integer id,
+                        Long empresaId,
+                        Integer estadoId);
 
 }
