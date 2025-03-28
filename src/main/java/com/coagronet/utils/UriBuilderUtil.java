4de9800d3bfce1030f3162c6f02fde6097eeb25a
Juan José Castro Gutiérrez <51551073+jujcgu@users.noreply.github.com>
2025-03-27 20:30:54 -0500

    ADD UriBuilderUtil

---
+++ src/main/java/com/coagronet/utils/UriBuilderUtil.java
@@ -0,0 +1,17 @@
+package com.coagronet.utils;
+
+import java.net.URI;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+public class UriBuilderUtil {
+
+    public URI buildTipoEvaluacionUri(Integer id, UriComponentsBuilder ucb) {
+        return ucb.path("/api/v1/tipo_evaluacion/{id}")
+                .buildAndExpand(id)
+                .toUri();
+    }
+
+}
