32fe61238c11de4dc1988d77126b5bf4fd21e5e6
jujcgu <juanjosecastrogutierrez@gmail.com>
2025-05-05 17:31:10 -0500

    refactor: reorganized import statements and corrected column name for usuarioEstado

--- src/main/java/com/coagronet/user/User.java
+++ src/main/java/com/coagronet/user/User.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +12,18 @@ import com.coagronet.persona.Persona;
 import com.coagronet.role.Role;
 import com.coagronet.usuarioEstado.UsuarioEstado;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -48,7 +59,7 @@ public class User implements UserDetails {
     private Set<Role> roles;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "usu_estado", referencedColumnName = "use_id")
+    @JoinColumn(name = "usu_estado_id", referencedColumnName = "use_id")
     private UsuarioEstado usuarioEstado;
 
     @Override
