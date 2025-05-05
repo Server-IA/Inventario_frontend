a2f5e710ba3a236a69ade32cf3f2ae4339242404
jujcgu <juanjosecastrogutierrez@gmail.com>
2025-05-05 17:40:39 -0500

    refactor: updated Pais entity to use Long for id and codigo, adjusted nombre length, and added relationships with Empresa and Estado

--- src/main/java/com/coagronet/pais/Pais.java
+++ src/main/java/com/coagronet/pais/Pais.java
@@ -1,10 +1,16 @@
 package com.coagronet.pais;
 
+import com.coagronet.empresa.Empresa;
+import com.coagronet.estado.Estado;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -26,18 +32,23 @@ public class Pais {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "paiSeqGen")
     @SequenceGenerator(name = "paiSeqGen", sequenceName = "pais_pai_id_seq", allocationSize = 1)
     @Column(name = "pai_id")
-    private Integer id;
+    private Long id;
 
-    @Column(name = "pai_nombre", length = 25, nullable = false)
+    @Column(name = "pai_nombre", length = 70, nullable = false)
     private String nombre;
 
     @Column(name = "pai_codigo", nullable = false)
-    private Integer codigo;
+    private Long codigo;
 
     @Column(name = "pai_acronimo", length = 3, nullable = false)
     private String acronimo;
 
-    @Column(name = "pai_propietario", nullable = false)
-    private Integer propietario;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pai_empresa_id", referencedColumnName = "emp_id", nullable = false)
+    private Empresa empresa;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pai_estado_id", referencedColumnName = "est_id", nullable = false)
+    private Estado estado;
 }
 
