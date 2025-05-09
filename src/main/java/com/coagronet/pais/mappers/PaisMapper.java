0f2e4c8f05f17a128f1eae75bda85bc15c07aa2f
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-09 00:48:56 -0500

    fix: add mappings for empresa and estado in PaisMapper

--- src/main/java/com/coagronet/pais/mappers/PaisMapper.java
+++ src/main/java/com/coagronet/pais/mappers/PaisMapper.java
@@ -2,13 +2,18 @@ package com.coagronet.pais.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
 import com.coagronet.pais.Pais;
 import com.coagronet.pais.dtos.PaisDTO;
 
 @Mapper(componentModel = "spring")
 public interface PaisMapper {
 
-    @Mapping(source = "nombre", target = "name")
+    @Mapping(source = "empresa.id", target = "empresaId")
+    @Mapping(source = "estado.id", target = "estadoId")
     PaisDTO toDTO(Pais pais);
+
+    @Mapping(source = "empresaId", target = "empresa.id")
+    @Mapping(source = "estadoId", target = "estado.id")
+    Pais toEntity(PaisDTO paisDTO);
+
 }
