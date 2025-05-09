ba3f1a6e7ddc4d6f7d155e007c3c039eb42b541d
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-08 19:36:46 -0500

    refactor: change id type to Long and update estado relationship in TipoIdentificacion

--- src/main/java/com/coagronet/tipoIdentificacion/TipoIdentificacion.java
+++ src/main/java/com/coagronet/tipoIdentificacion/TipoIdentificacion.java
@@ -1,11 +1,18 @@
 package com.coagronet.tipoIdentificacion;
 
+import com.coagronet.estado.Estado;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,18 +25,21 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "tipo_identificacion")
 public class TipoIdentificacion {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "tii_id")
-    private Integer id;
 
-    @Column(name = "tii_nombre")
+    @Id
+    @SequenceGenerator(name = "tipo_identificacion_sequence", sequenceName = "tipo_identificacion_tii_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tipo_identificacion_sequence")
+    @Column(name = "tii_id")
+    private Long id;
+
+    @Column(name = "tii_nombre", length = 255)
     private String nombre;
 
-    @Column(name = "tii_descripcion")
+    @Column(name = "tii_descripcion", length = 255)
     private String descripcion;
 
-    @Column(name = "tii_estado")
-    private Integer estado;
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tii_estado_id", referencedColumnName = "est_id", nullable = false)
+    private Estado estado;
+    
 }
