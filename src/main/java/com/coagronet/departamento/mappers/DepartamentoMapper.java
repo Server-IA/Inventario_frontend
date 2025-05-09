08c8ed633122a66d61f447631a7e00b44da814fa
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-08 23:23:26 -0500

    refactor: update DepartamentoMapper to include missing mappings for nombre field

--- src/main/java/com/coagronet/departamento/mappers/DepartamentoMapper.java
+++ src/main/java/com/coagronet/departamento/mappers/DepartamentoMapper.java
@@ -2,6 +2,7 @@ package com.coagronet.departamento.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 import com.coagronet.departamento.Departamento;
 import com.coagronet.departamento.dtos.DepartamentoDTO;
@@ -9,6 +10,16 @@ import com.coagronet.departamento.dtos.DepartamentoDTO;
 @Mapper(componentModel = "spring")
 public interface DepartamentoMapper {
 
-    @Mapping(source = "nombre", target="name")
+    DepartamentoMapper INSTANCE = Mappers.getMapper(DepartamentoMapper.class);
+
+    @Mapping(source = "pais.id", target = "paisId")
+    @Mapping(source = "empresa.id", target = "empresaId")
+    @Mapping(source = "estado.id", target = "estadoId")
     DepartamentoDTO toDTO(Departamento departamento);
+
+    @Mapping(source = "paisId", target = "pais.id")
+    @Mapping(source = "empresaId", target = "empresa.id")
+    @Mapping(source = "estadoId", target = "estado.id")
+    Departamento toEntity(DepartamentoDTO departamentoDTO);
+
 }
