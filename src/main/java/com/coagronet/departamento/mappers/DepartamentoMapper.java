f981450dca5ab2423b6020252bbedde361adcbfe
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-09 01:43:54 -0500

    fix: remove unused Mappers factory instance from DepartamentoMapper

--- src/main/java/com/coagronet/departamento/mappers/DepartamentoMapper.java
+++ src/main/java/com/coagronet/departamento/mappers/DepartamentoMapper.java
@@ -2,15 +2,12 @@ package com.coagronet.departamento.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 import com.coagronet.departamento.Departamento;
 import com.coagronet.departamento.dtos.DepartamentoDTO;
 
 @Mapper(componentModel = "spring")
 public interface DepartamentoMapper {
-
-    DepartamentoMapper INSTANCE = Mappers.getMapper(DepartamentoMapper.class);
 
     @Mapping(source = "pais.id", target = "paisId")
     @Mapping(source = "empresa.id", target = "empresaId")
