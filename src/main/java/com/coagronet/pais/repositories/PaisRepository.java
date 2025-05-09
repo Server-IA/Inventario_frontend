32a8008408ffe974244ef9afcb2ee469fa186a76
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-08 23:22:44 -0500

    refactor: update PaisRepository to use Long for ID type and add methods for finding and checking existence by empresaId

--- src/main/java/com/coagronet/pais/repositories/PaisRepository.java
+++ src/main/java/com/coagronet/pais/repositories/PaisRepository.java
@@ -1,11 +1,21 @@
 package com.coagronet.pais.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.coagronet.pais.Pais;
 
 @Repository
-public interface PaisRepository extends JpaRepository<Pais, Integer>{
+public interface PaisRepository extends JpaRepository<Pais, Long> {
+
+    Optional<Pais> findByIdAndEmpresaId(
+            Long id,
+            Long empresaId);
+
+    boolean existsByIdAndEmpresaId(
+            Long id,
+            Long empresaId);
 
 }
