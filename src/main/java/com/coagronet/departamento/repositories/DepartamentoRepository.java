9bd31d74739f0a16cd8eb3b78b00ba967f20c006
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-08 23:23:42 -0500

    refactor: update DepartamentoRepository to use Long for ID type and add methods for finding by empresaId

--- src/main/java/com/coagronet/departamento/repositories/DepartamentoRepository.java
+++ src/main/java/com/coagronet/departamento/repositories/DepartamentoRepository.java
@@ -1,11 +1,29 @@
 package com.coagronet.departamento.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.coagronet.departamento.Departamento;
 
 @Repository
-public interface DepartamentoRepository extends JpaRepository<Departamento, Integer> {
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+
+    Optional<Departamento> findByIdAndEmpresaId(
+            Long id,
+            Long empresaId);
+
+    List<Departamento> findByEstadoIdNotOrderByIdAsc(
+            Long estadoId);
+
+    boolean existsByIdAndEmpresaId(
+            Long id,
+            Long empresaId);
+
+    boolean existsByIdAndEstadoIdNot(
+            Long id,
+            Long estadoId);
 
 }
