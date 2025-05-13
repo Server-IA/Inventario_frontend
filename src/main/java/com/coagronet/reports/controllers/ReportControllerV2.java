8582c5307bc70aec19812e7f0ee45d77629995e5
Zenway <zenway15@gmail.com>
2025-05-13 18:08:14 -0500

    refactor: se eliminan importaciones inutiles.

--- src/main/java/com/coagronet/reports/controllers/ReportControllerV2.java
+++ src/main/java/com/coagronet/reports/controllers/ReportControllerV2.java
@@ -1,6 +1,5 @@
 package com.coagronet.reports.controllers;
 
-import net.sf.jasperreports.engine.*;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,11 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import com.coagronet.reports.services.ReportService;
 
-import javax.sql.DataSource;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,13 +16,11 @@ import java.util.Map;
 public class ReportControllerV2 {
 
     private final ReportService reportService;
-    private final DataSource dataSource;
 
 
 
-    public ReportControllerV2(ReportService reportService, DataSource dataSource) {
+    public ReportControllerV2(ReportService reportService) {
         this.reportService = reportService;
-        this.dataSource = dataSource;
     }
 
     @PostMapping("/{reportName}")
