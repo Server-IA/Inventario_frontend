1f1376c1ecb20989a5538aace8e5c56ebf6050ff
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-15 14:04:26 -0500

    refactor: update MunicipioRepository to use Long as ID type and add query methods

--- src/main/java/com/coagronet/municipio/repositories/MunicipioRepository.java
+++ src/main/java/com/coagronet/municipio/repositories/MunicipioRepository.java
@@ -1,11 +1,23 @@
 package com.coagronet.municipio.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.coagronet.municipio.Municipio;
 
 @Repository
-public interface MunicipioRepository extends JpaRepository<Municipio, Integer> {
+public interface MunicipioRepository extends JpaRepository<Municipio, Long> {
+
+	Optional<Municipio> findByIdAndEmpresaId(Long id, Long empresaId);
+
+	List<Municipio> findByDepartamentoIdAndEmpresaIdOrderByIdAsc(Long departamentoId, Long empresaId);
+
+	List<Municipio> findByDepartamentoIdAndEmpresaIdAndEstadoIdNotOrderByIdAsc(Long departamentoId, Long empresaId,
+			Long estadoId);
+
+	boolean existsByIdAndDepartamentoIdAndEmpresaId(Long id, Long departamentoId, Long empresaId);
 
 }
