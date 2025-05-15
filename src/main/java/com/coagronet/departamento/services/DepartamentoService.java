236ee52cd6853692e1b565a93cf2f5eee3fc4cd3
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-15 15:32:35 -0500

    refactor: improve method formatting and consistency in DepartamentoService

--- src/main/java/com/coagronet/departamento/services/DepartamentoService.java
+++ src/main/java/com/coagronet/departamento/services/DepartamentoService.java
@@ -24,84 +24,75 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DepartamentoService {
 
-    private final DepartamentoMapper departamentoMapper;
-    private final DepartamentoRepository departamentoRepository;
-    private final PaisRepository paisRepository;
-    private final AuthenticationService authenticationService;
-    private final UserEmpresaService userEmpresaService;
-    private final EstadoRepository estadoRepository;
+	private final DepartamentoMapper departamentoMapper;
+	private final DepartamentoRepository departamentoRepository;
+	private final PaisRepository paisRepository;
+	private final AuthenticationService authenticationService;
+	private final UserEmpresaService userEmpresaService;
+	private final EstadoRepository estadoRepository;
 
-    public List<DepartamentoDTO> findAll() {
-        User user = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-        return departamentoRepository.findByEmpresaIdOrderByIdAsc(empresa.getId())
-                .stream()
-                .map(departamentoMapper::toDTO)
-                .collect(Collectors.toList());
-    }
+	public List<DepartamentoDTO> findAll() {
+		User user = authenticationService.getAuthenticatedUser();
+		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
+		return departamentoRepository.findByEmpresaIdOrderByIdAsc(empresa.getId()).stream()
+				.map(departamentoMapper::toListDto).collect(Collectors.toList());
+	}
 
-    public List<DepartamentoDTO> findAllAvailable() {
-        User user = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-        return departamentoRepository.findByEmpresaIdAndEstadoIdNotOrderByIdAsc(empresa.getId(), 2L)
-                .stream()
-                .map(departamentoMapper::toDTO)
-                .collect(Collectors.toList());
-    }
+	public List<DepartamentoDTO> findAllAvailable() {
+		User user = authenticationService.getAuthenticatedUser();
+		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
+		return departamentoRepository.findByEmpresaIdAndEstadoIdNotOrderByIdAsc(empresa.getId(), 2L).stream()
+				.map(departamentoMapper::toListDto).collect(Collectors.toList());
+	}
 
-    public Optional<DepartamentoDTO> findById(Long requestedId) {
-        User user = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-        return departamentoRepository.findByIdAndEmpresaId(requestedId, empresa.getId())
-                .map(departamentoMapper::toDTO);
-    }
+	public Optional<DepartamentoDTO> findById(Long requestedId) {
+		User user = authenticationService.getAuthenticatedUser();
+		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
+		return departamentoRepository.findByIdAndEmpresaId(requestedId, empresa.getId())
+				.map(departamentoMapper::toListDto);
+	}
 
-    public DepartamentoDTO create(DepartamentoDTO departamentoDTO) {
+	public DepartamentoDTO create(DepartamentoDTO departamentoDTO) {
 
-        paisRepository.findByIdAndEmpresaId(departamentoDTO.getPaisId(), departamentoDTO.getEmpresaId())
-                .orElseThrow(
-                        () -> new BadRequestException("El país no es válido para la empresa del usuario autenticado"));
+		paisRepository.findByIdAndEmpresaId(departamentoDTO.getPaisId(), departamentoDTO.getEmpresaId()).orElseThrow(
+				() -> new BadRequestException("El país no es válido para la empresa del usuario autenticado"));
 
-        estadoRepository.findById(departamentoDTO.getEstadoId())
-                .orElseThrow(() -> new BadRequestException("El estado no es válido"));
+		estadoRepository.findById(departamentoDTO.getEstadoId())
+				.orElseThrow(() -> new BadRequestException("El estado no es válido"));
 
-        User user = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
+		User user = authenticationService.getAuthenticatedUser();
+		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
 
-        departamentoDTO.setId(null);
-        departamentoDTO.setEmpresaId(empresa.getId());
+		departamentoDTO.setId(null);
+		departamentoDTO.setEmpresaId(empresa.getId());
 
-        return departamentoMapper.toDTO(
-                departamentoRepository.save(
-                        departamentoMapper.toEntity(departamentoDTO)));
-    }
+		return departamentoMapper.toDTO(departamentoRepository.save(departamentoMapper.toEntity(departamentoDTO)));
+	}
 
-    public void update(Long requestedId, DepartamentoDTO departamentoDTO) {
-        User user = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
+	public void update(Long requestedId, DepartamentoDTO departamentoDTO) {
+		User user = authenticationService.getAuthenticatedUser();
+		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
 
-        departamentoRepository.findByIdAndEmpresaId(requestedId, empresa.getId())
-                .orElseThrow(() -> new NotFoundException("Departamento no encontrado"));
+		departamentoRepository.findByIdAndEmpresaId(requestedId, empresa.getId())
+				.orElseThrow(() -> new NotFoundException("Departamento no encontrado"));
 
-        paisRepository.findByIdAndEmpresaId(departamentoDTO.getPaisId(), empresa.getId())
-                .orElseThrow(
-                        () -> new BadRequestException("El país no es válido para la empresa del usuario autenticado"));
+		paisRepository.findByIdAndEmpresaId(departamentoDTO.getPaisId(), empresa.getId()).orElseThrow(
+				() -> new BadRequestException("El país no es válido para la empresa del usuario autenticado"));
 
-        departamentoDTO.setId(requestedId);
-        departamentoDTO.setEmpresaId(empresa.getId());
+		departamentoDTO.setId(requestedId);
+		departamentoDTO.setEmpresaId(empresa.getId());
 
-        departamentoRepository.save(
-                departamentoMapper.toEntity(departamentoDTO));
-    }
+		departamentoRepository.save(departamentoMapper.toEntity(departamentoDTO));
+	}
 
-    public void delete(Long id) {
-        User user = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
+	public void delete(Long id) {
+		User user = authenticationService.getAuthenticatedUser();
+		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
 
-        departamentoRepository.findByIdAndEmpresaId(id, empresa.getId())
-                .orElseThrow(() -> new NotFoundException("Departamento no encontrado"));
+		departamentoRepository.findByIdAndEmpresaId(id, empresa.getId())
+				.orElseThrow(() -> new NotFoundException("Departamento no encontrado"));
 
-        departamentoRepository.deleteById(id);
-    }
+		departamentoRepository.deleteById(id);
+	}
 
 }
