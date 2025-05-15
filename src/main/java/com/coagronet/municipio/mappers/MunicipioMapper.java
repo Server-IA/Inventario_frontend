fe957ad658aa399fc15841a098b3ee6507b58b45
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-15 14:04:14 -0500

    refactor: update MunicipioMapper mappings for consistency and clarity

--- src/main/java/com/coagronet/municipio/mappers/MunicipioMapper.java
+++ src/main/java/com/coagronet/municipio/mappers/MunicipioMapper.java
@@ -9,6 +9,22 @@ import com.coagronet.municipio.dtos.MunicipioDTO;
 @Mapper(componentModel = "spring")
 public interface MunicipioMapper {
 
-    @Mapping(source = "nombre", target = "name")
-    MunicipioDTO toDTO(Municipio municipio);
+	@Mapping(source = "departamento.id", target = "departamentoId")
+	@Mapping(source = "empresa.id", target = "empresaId")
+	@Mapping(source = "estado.id", target = "estadoId")
+	MunicipioDTO toDTO(Municipio municipio);
+
+	@Mapping(source = "departamentoId", target = "departamento.id")
+	@Mapping(source = "empresaId", target = "empresa.id")
+	@Mapping(source = "estadoId", target = "estado.id")
+	Municipio toEntity(MunicipioDTO municipioDTO);
+
+	@Mapping(target = "id", source = "id")
+	@Mapping(target = "nombre", source = "nombre")
+	@Mapping(target = "departamentoId", source = "departamento.id")
+	@Mapping(target = "codigo", source = "codigo")
+	@Mapping(target = "acronimo", source = "acronimo")
+	@Mapping(target = "empresaId", ignore = true)
+	@Mapping(target = "estadoId", source = "estado.id")
+	MunicipioDTO toListDto(Municipio municipio);
 }
