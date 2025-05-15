88872e61ed01e6e4cefcf270e030f5baa59e0b5c
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-15 14:04:46 -0500

    refactor: clean up imports and format MarcaController for improved readability

--- src/main/java/com/coagronet/marca/controllers/MarcaController.java
+++ src/main/java/com/coagronet/marca/controllers/MarcaController.java
@@ -2,7 +2,6 @@ package com.coagronet.marca.controllers;
 
 import com.coagronet.utils.UriBuilderUtil;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -29,52 +29,41 @@ import com.coagronet.marca.services.MarcaService;
 @RequiredArgsConstructor
 public class MarcaController {
 
-        private final MarcaService marcaService;
-        private final UriBuilderUtil uriBuilderUtil;
+	private final MarcaService marcaService;
+	private final UriBuilderUtil uriBuilderUtil;
 
-        @GetMapping
-        public ResponseEntity<List<MarcaDTO>> findAll() {
-                return ResponseEntity.ok(marcaService.findAll());
-        }
+	@GetMapping
+	public ResponseEntity<List<MarcaDTO>> findAll() {
+		return ResponseEntity.ok(marcaService.findAll());
+	}
 
-        @GetMapping(params = "available=true")
-        public ResponseEntity<List<MarcaDTO>> findAllAvailable() {
-                return ResponseEntity.ok(marcaService.findAllAvailable());
-        }
+	@GetMapping(params = "available=true")
+	public ResponseEntity<List<MarcaDTO>> findAllAvailable() {
+		return ResponseEntity.ok(marcaService.findAllAvailable());
+	}
 
-        @GetMapping("/{requestedId}")
-        public ResponseEntity<MarcaDTO> findById(
-                        @PathVariable Long requestedId) {
-                return marcaService.findById(requestedId)
-                                .map(ResponseEntity::ok)
-                                .orElse(ResponseEntity.notFound().build());
-        }
+	@GetMapping("/{requestedId}")
+	public ResponseEntity<MarcaDTO> findById(@PathVariable Long requestedId) {
+		return marcaService.findById(requestedId).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+	}
 
-        @PostMapping
-        public ResponseEntity<Void> createMarca(
-                        @Valid @RequestBody MarcaDTO marcaDTO,
-                        UriComponentsBuilder ucb) {
-                MarcaDTO savedMarca = marcaService.create(
-                                marcaDTO);
-                URI locationOfNewMarca = uriBuilderUtil.buildMarcaUri(
-                                savedMarca.getId(),
-                                ucb);
-                return ResponseEntity.created(locationOfNewMarca).build();
-        }
+	@PostMapping
+	public ResponseEntity<Void> createMarca(@Valid @RequestBody MarcaDTO marcaDTO, UriComponentsBuilder ucb) {
+		MarcaDTO savedMarca = marcaService.create(marcaDTO);
+		URI locationOfNewMarca = uriBuilderUtil.buildMarcaUri(savedMarca.getId(), ucb);
+		return ResponseEntity.created(locationOfNewMarca).build();
+	}
 
-        @PutMapping("/{requestedId}")
-        public ResponseEntity<Void> updateMarca(
-                        @PathVariable Long requestedId,
-                        @Valid @RequestBody MarcaDTO marcaDTO) {
-                marcaService.update(requestedId, marcaDTO);
-                return ResponseEntity.noContent().build();
-        }
+	@PutMapping("/{requestedId}")
+	public ResponseEntity<Void> updateMarca(@PathVariable Long requestedId, @Valid @RequestBody MarcaDTO marcaDTO) {
+		marcaService.update(requestedId, marcaDTO);
+		return ResponseEntity.noContent().build();
+	}
 
-        @DeleteMapping("/{id}")
-        public ResponseEntity<Void> deleteMarca(
-                        @PathVariable Long id) {
-                marcaService.delete(id);
-                return ResponseEntity.noContent().build();
-        }
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteMarca(@PathVariable Long id) {
+		marcaService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
