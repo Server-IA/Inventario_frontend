de9f96d18dd8351bbbaf369b983658b9b8810e32
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-15 15:32:27 -0500

    refactor: standardize formatting in DepartamentoMapper for consistency

--- src/main/java/com/coagronet/departamento/mappers/DepartamentoMapper.java
+++ src/main/java/com/coagronet/departamento/mappers/DepartamentoMapper.java
@@ -9,14 +9,23 @@ import com.coagronet.departamento.dtos.DepartamentoDTO;
 @Mapper(componentModel = "spring")
 public interface DepartamentoMapper {
 
-    @Mapping(source = "pais.id", target = "paisId")
-    @Mapping(source = "empresa.id", target = "empresaId")
-    @Mapping(source = "estado.id", target = "estadoId")
-    DepartamentoDTO toDTO(Departamento departamento);
+	@Mapping(source = "pais.id", target = "paisId")
+	@Mapping(source = "empresa.id", target = "empresaId")
+	@Mapping(source = "estado.id", target = "estadoId")
+	DepartamentoDTO toDTO(Departamento departamento);
 
-    @Mapping(source = "paisId", target = "pais.id")
-    @Mapping(source = "empresaId", target = "empresa.id")
-    @Mapping(source = "estadoId", target = "estado.id")
-    Departamento toEntity(DepartamentoDTO departamentoDTO);
+	@Mapping(source = "paisId", target = "pais.id")
+	@Mapping(source = "empresaId", target = "empresa.id")
+	@Mapping(source = "estadoId", target = "estado.id")
+	Departamento toEntity(DepartamentoDTO departamentoDTO);
+
+	@Mapping(source = "id", target = "id")
+	@Mapping(source = "nombre", target = "nombre")
+	@Mapping(target = "paisId", source = "pais.id")
+	@Mapping(source = "codigo", target = "codigo")
+	@Mapping(source = "acronimo", target = "acronimo")
+	@Mapping(target = "empresaId", ignore = true)
+	@Mapping(target = "estadoId", source = "estado.id")
+	DepartamentoDTO toListDto(Departamento departamento);
 
 }
