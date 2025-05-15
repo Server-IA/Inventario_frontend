6523ba7268fd30bdea09522e28173ece88220b30
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-15 15:32:22 -0500

    refactor: format DepartamentoDTO for improved readability and maintainability

--- src/main/java/com/coagronet/departamento/dtos/DepartamentoDTO.java
+++ src/main/java/com/coagronet/departamento/dtos/DepartamentoDTO.java
@@ -1,5 +1,7 @@
 package com.coagronet.departamento.dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -11,27 +13,28 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class DepartamentoDTO {
 
-    private Long id;
+	private Long id;
 
-    @NotBlank(message = "El nombre es obligatorio.")
-    @Size(max = 70, message = "El nombre no debe superar los 70 caracteres.")
-    private String nombre;
+	@NotBlank(message = "El nombre es obligatorio.")
+	@Size(max = 70, message = "El nombre no debe superar los 70 caracteres.")
+	private String nombre;
 
-    @NotNull(message = "El pais es obligatorio.")
-    private Long paisId;
+	@NotNull(message = "El pais es obligatorio.")
+	private Long paisId;
 
-    @NotNull(message = "El código no puede ser nulo")
-    private Integer codigo;
+	@NotNull(message = "El código no puede ser nulo")
+	private Integer codigo;
 
-    @NotBlank(message = "El acronimo es obligatorio.")
-    @Size(max = 3, message = "El acronimo no debe superar los 3 caracteres.")
-    private String acronimo;
+	@NotBlank(message = "El acronimo es obligatorio.")
+	@Size(max = 3, message = "El acronimo no debe superar los 3 caracteres.")
+	private String acronimo;
 
-    private Long empresaId;
+	private Long empresaId;
 
-    @NotNull(message = "El estado es obligatorio.")
-    private Long estadoId;
+	@NotNull(message = "El estado es obligatorio.")
+	private Long estadoId;
 
 }
