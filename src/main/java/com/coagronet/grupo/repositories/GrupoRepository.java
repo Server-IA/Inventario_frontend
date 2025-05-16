5fb3d7b89ef2f199c4c8890ed97680b22ae3a372
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-16 16:20:10 -0500

    refactor: streamline GrupoRepository methods for improved clarity and consistency

--- src/main/java/com/coagronet/grupo/repositories/GrupoRepository.java
+++ src/main/java/com/coagronet/grupo/repositories/GrupoRepository.java
@@ -3,26 +3,18 @@ package com.coagronet.grupo.repositories;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
 
-import com.coagronet.empresa.Empresa;
 import com.coagronet.grupo.Grupo;
 
 @Repository
 public interface GrupoRepository extends JpaRepository<Grupo, Long> {
 
-    Page<Grupo> findById(Long id, PageRequest pageRequest);
+	Optional<Grupo> findByIdAndEmpresaId(Long id, Long empresaId);
 
-    List<Grupo> findByEstadoNotAndEmpresa(Integer estado, Empresa empresa, Sort sort);
+	List<Grupo> findByEmpresaIdOrderByIdAsc(Long empresaId);
 
-    Optional<Grupo> findByIdAndEmpresaIdAndEstadoIdNot(Long id, Long empresaId, Long estadoId);
+	List<Grupo> findByEmpresaIdAndEstadoIdNotOrderByIdAsc(Long empresaId, Long estadoId);
 
-// Ejemplo con query nativa
-// @Query(value = "select * from grupo g where gru_id = :grupoId and gru_empresa_id = :empresaId and gru_estado_id <> :estadoId", nativeQuery = true)
-//    Optional<Grupo> buscarPorIdYEmpresaIdYEstadoEsDiferenteDeInactivo(@Param("grupoId") Long grupoId, @Param("empresaId") Long empresaId, @Param("estadoId") Long estadoId);
 }
