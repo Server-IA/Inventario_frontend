c0e6d882e39994c616ab48fc99a5ef237e74aa96
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-16 16:19:55 -0500

    refactor: simplify GrupoMapper methods and improve mapping consistency

--- src/main/java/com/coagronet/grupo/mappers/GrupoMapper.java
+++ src/main/java/com/coagronet/grupo/mappers/GrupoMapper.java
@@ -1,31 +1,23 @@
 package com.coagronet.grupo.mappers;
 
-
 import com.coagronet.grupo.Grupo;
 import com.coagronet.grupo.dtos.GrupoDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
-
 public interface GrupoMapper {
 
-    GrupoMapper INSTANCE = Mappers.getMapper(GrupoMapper.class);
+	@Mapping(source = "empresa.id", target = "empresaId")
+	@Mapping(source = "estado.id", target = "estadoId")
+	GrupoDTO toDTO(Grupo grupo);
 
-    @Mapping(source = "empresa.id", target = "empresa")
-    @Mapping(source = "estado.id", target = "estado")
-    GrupoDTO toDTO(Grupo grupo);
+	@Mapping(source = "empresaId", target = "empresa.id")
+	@Mapping(source = "estadoId", target = "estado.id")
+	Grupo toEntity(GrupoDTO grupoDTO);
 
-    @Mapping(target = "empresa", ignore = true)
-    @Mapping(target = "descripcion", ignore = true)
-    @Mapping(target = "estado", ignore = true)
-    @Mapping(target = "id", source = "id")
-    @Mapping(target = "nombre", source = "nombre")
-    GrupoDTO toMinimalDTO(Grupo grupo);
-
-    @Mapping(source = "empresa", target = "empresa.id")
-    @Mapping(source = "estado", target = "estado.id")
-    Grupo toEntity(GrupoDTO grupoDTO);
+	@Mapping(target = "empresaId", ignore = true)
+	@Mapping(target = "estadoId", source = "estado.id")
+	GrupoDTO toListDto(Grupo grupo);
 
 }
