b24ef90c9d2478d0281ec19ae6d28db63c0d5631
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-20 11:20:54 -0500

    refactor: comment out methods in OrdenCompraController for future implementation

--- src/main/java/com/coagronet/ordenCompra/controllers/OrdenCompraController.java
+++ src/main/java/com/coagronet/ordenCompra/controllers/OrdenCompraController.java
@@ -35,136 +35,104 @@ import com.coagronet.userRole.repositories.UserRoleRepository;
 @RestController
 @RequestMapping("/api/v1/orden_compra")
 @CrossOrigin(origins = "*")
-public class OrdenCompraController {
-
-    private final OrdenCompraRepository ordenCompraRepository;
-    private final OrdenCompraMapper ordenCompraMapper;
-    private final UserRoleRepository userRoleRepository;
-    private final UserRepository userRepository;
-    private final PedidoRepository pedidoRepository;
-    private final ProveedorRepository proveedorRepository;
-
-    private OrdenCompraController(
-            OrdenCompraRepository ordenCompraRepository,
-            OrdenCompraMapper ordenCompraMapper,
-            UserRoleRepository userRoleRepository,
-            UserRepository userRepository,
-            PedidoRepository pedidoRepository,
-            ProveedorRepository proveedorRepository) {
-        this.ordenCompraRepository = ordenCompraRepository;
-        this.ordenCompraMapper = ordenCompraMapper;
-        this.userRoleRepository = userRoleRepository;
-        this.userRepository = userRepository;
-        this.pedidoRepository = pedidoRepository;
-        this.proveedorRepository = proveedorRepository;
-    }
-
-    private Empresa getEmpresaFromUser(User user) {
-        return userRoleRepository.findByUser(user).stream()
-                .map(UserRole::getEmpresa)
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("Empresa no encontrada para el usuario"));
-    }
-
-    private User getAuthenticatedUser() {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        return userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
-    }
-
-    @GetMapping("/{requestedId}")
-    private ResponseEntity<OrdenCompraDTO> findById(@PathVariable Long requestedId) {
-        User authenticatedUser = getAuthenticatedUser();
-        Empresa empresa = getEmpresaFromUser(authenticatedUser);
-        return ordenCompraRepository
-                .findByIdAndPedidoAlmacenSedeEmpresaId(requestedId, empresa.getId())
-                .map(ordenCompraMapper::toDTO)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-    @PostMapping
-    private ResponseEntity<Void> createOrdenCompra(@RequestBody OrdenCompraDTO newOrdenCompraRequest,
-            UriComponentsBuilder ucb) {
-        OrdenCompraDTO newOrdenCompra = new OrdenCompraDTO(
-                null,
-                newOrdenCompraRequest.getFechaHora(),
-                newOrdenCompraRequest.getPedido(),
-                newOrdenCompraRequest.getProveedor(),
-                newOrdenCompraRequest.getDescripcion(),
-                newOrdenCompraRequest.getEstado());
-        User authenticatedUser = getAuthenticatedUser();
-        Empresa empresa = getEmpresaFromUser(authenticatedUser);
-        if (pedidoRepository.existsByIdAndAlmacenSedeEmpresaIdAndEstadoIdNot(
-                newOrdenCompraRequest.getPedido(), empresa.getId(), 2)
-                && proveedorRepository.existsByIdAndEmpresaIdAndEstadoIdNot(
-                        newOrdenCompraRequest.getProveedor(), empresa.getId(), 2)) {
-            OrdenCompra savedOrdenCompra = ordenCompraMapper.toEntity(newOrdenCompra);
-            ordenCompraRepository.save(savedOrdenCompra);
-            URI locationOfNewOrdenCompra = ucb
-                    .path("/api/v1/orden_compra/{id}")
-                    .buildAndExpand(savedOrdenCompra.getId())
-                    .toUri();
-            return ResponseEntity.created(locationOfNewOrdenCompra).build();
-        }
-        return ResponseEntity.badRequest().build();
-    }
-
-    @GetMapping("/pedidoId/{requestedPedidoId}")
-    private ResponseEntity<List<OrdenCompraDTO>> findAllByPedidoId(@PathVariable Integer requestedPedidoId,
-            @PageableDefault Pageable pageable) {
-        User authenticatedUser = getAuthenticatedUser();
-        Empresa empresa = getEmpresaFromUser(authenticatedUser);
-        List<OrdenCompraDTO> ordenCompraDTOs = ordenCompraRepository
-                .findByPedidoIdAndPedidoAlmacenSedeEmpresaIdAndEstadoIdNotOrderByIdAsc(
-                        requestedPedidoId, empresa.getId(), 2)
-                .stream()
-                .map(ordenCompraMapper::toDTO)
-                .collect(Collectors.toList());
-
-        return !ordenCompraDTOs.isEmpty()
-                ? ResponseEntity.ok(ordenCompraDTOs)
-                : ResponseEntity.noContent().build();
-    }
-
-    @PutMapping("/{requestedId}")
-    private ResponseEntity<Void> putOrdenCompra(
-            @PathVariable Long requestedId, @RequestBody OrdenCompraDTO ordenCompraDTOUpdate) {
-        User authenticatedUser = getAuthenticatedUser();
-        Empresa empresa = getEmpresaFromUser(authenticatedUser);
-        if (ordenCompraRepository.existsByIdAndPedidoAlmacenSedeEmpresaId(
-                requestedId, empresa.getId())
-                && pedidoRepository.existsByIdAndAlmacenSedeEmpresaIdAndEstadoIdNot(
-                        ordenCompraDTOUpdate.getPedido(), empresa.getId(), 2)
-                && proveedorRepository.existsByIdAndEmpresaIdAndEstadoIdNot(
-                        ordenCompraDTOUpdate.getProveedor(), empresa.getId(), 2)) {
-            OrdenCompraDTO updateOrdenCompraDTO = new OrdenCompraDTO(
-                    requestedId,
-                    ordenCompraDTOUpdate.getFechaHora(),
-                    ordenCompraDTOUpdate.getPedido(),
-                    ordenCompraDTOUpdate.getProveedor(),
-                    ordenCompraDTOUpdate.getDescripcion(),
-                    ordenCompraDTOUpdate.getEstado());
-            OrdenCompra updatedOrdenCompra = ordenCompraMapper.toEntity(updateOrdenCompraDTO);
-            ordenCompraRepository.save(updatedOrdenCompra);
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
-    }
-
-    @DeleteMapping("/{id}")
-    private ResponseEntity<Void> deleteOrdenCompra(@PathVariable Long id) {
-        try {
-            User authenticatedUser = getAuthenticatedUser();
-            Empresa empresa = getEmpresaFromUser(authenticatedUser);
-            if (ordenCompraRepository.existsByIdAndPedidoAlmacenSedeEmpresaId(id, empresa.getId())) {
-                ordenCompraRepository.deleteById(id);
-                return ResponseEntity.noContent().build();
-            }
-            return ResponseEntity.notFound().build();
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().build();
-        }
-    }
+public class OrdenCompraController {/*
+									 * 
+									 * private final OrdenCompraRepository ordenCompraRepository; private final
+									 * OrdenCompraMapper ordenCompraMapper; private final UserRoleRepository
+									 * userRoleRepository; private final UserRepository userRepository; private
+									 * final PedidoRepository pedidoRepository; private final ProveedorRepository
+									 * proveedorRepository;
+									 * 
+									 * private OrdenCompraController( OrdenCompraRepository ordenCompraRepository,
+									 * OrdenCompraMapper ordenCompraMapper, UserRoleRepository userRoleRepository,
+									 * UserRepository userRepository, PedidoRepository pedidoRepository,
+									 * ProveedorRepository proveedorRepository) { this.ordenCompraRepository =
+									 * ordenCompraRepository; this.ordenCompraMapper = ordenCompraMapper;
+									 * this.userRoleRepository = userRoleRepository; this.userRepository =
+									 * userRepository; this.pedidoRepository = pedidoRepository;
+									 * this.proveedorRepository = proveedorRepository; }
+									 * 
+									 * private Empresa getEmpresaFromUser(User user) { return
+									 * userRoleRepository.findByUser(user).stream() .map(UserRole::getEmpresa)
+									 * .findFirst() .orElseThrow(() -> new
+									 * RuntimeException("Empresa no encontrada para el usuario")); }
+									 * 
+									 * private User getAuthenticatedUser() { String username =
+									 * SecurityContextHolder.getContext().getAuthentication().getName(); return
+									 * userRepository.findByUsername(username) .orElseThrow(() -> new
+									 * UsernameNotFoundException("Usuario no encontrado")); }
+									 * 
+									 * @GetMapping("/{requestedId}") private ResponseEntity<OrdenCompraDTO>
+									 * findById(@PathVariable Long requestedId) { User authenticatedUser =
+									 * getAuthenticatedUser(); Empresa empresa =
+									 * getEmpresaFromUser(authenticatedUser); return ordenCompraRepository
+									 * .findByIdAndPedidoAlmacenSedeEmpresaId(requestedId, empresa.getId())
+									 * .map(ordenCompraMapper::toDTO) .map(ResponseEntity::ok)
+									 * .orElse(ResponseEntity.notFound().build()); }
+									 * 
+									 * @PostMapping private ResponseEntity<Void> createOrdenCompra(@RequestBody
+									 * OrdenCompraDTO newOrdenCompraRequest, UriComponentsBuilder ucb) {
+									 * OrdenCompraDTO newOrdenCompra = new OrdenCompraDTO( null,
+									 * newOrdenCompraRequest.getFechaHora(), newOrdenCompraRequest.getPedido(),
+									 * newOrdenCompraRequest.getProveedor(), newOrdenCompraRequest.getDescripcion(),
+									 * newOrdenCompraRequest.getEstado()); User authenticatedUser =
+									 * getAuthenticatedUser(); Empresa empresa =
+									 * getEmpresaFromUser(authenticatedUser); if
+									 * (pedidoRepository.existsByIdAndAlmacenSedeEmpresaIdAndEstadoIdNot(
+									 * newOrdenCompraRequest.getPedido(), empresa.getId(), 2) &&
+									 * proveedorRepository.existsByIdAndEmpresaIdAndEstadoIdNot(
+									 * newOrdenCompraRequest.getProveedor(), empresa.getId(), 2)) { OrdenCompra
+									 * savedOrdenCompra = ordenCompraMapper.toEntity(newOrdenCompra);
+									 * ordenCompraRepository.save(savedOrdenCompra); URI locationOfNewOrdenCompra =
+									 * ucb .path("/api/v1/orden_compra/{id}")
+									 * .buildAndExpand(savedOrdenCompra.getId()) .toUri(); return
+									 * ResponseEntity.created(locationOfNewOrdenCompra).build(); } return
+									 * ResponseEntity.badRequest().build(); }
+									 * 
+									 * @GetMapping("/pedidoId/{requestedPedidoId}") private
+									 * ResponseEntity<List<OrdenCompraDTO>> findAllByPedidoId(@PathVariable Integer
+									 * requestedPedidoId,
+									 * 
+									 * @PageableDefault Pageable pageable) { User authenticatedUser =
+									 * getAuthenticatedUser(); Empresa empresa =
+									 * getEmpresaFromUser(authenticatedUser); List<OrdenCompraDTO> ordenCompraDTOs =
+									 * ordenCompraRepository
+									 * .findByPedidoIdAndPedidoAlmacenSedeEmpresaIdAndEstadoIdNotOrderByIdAsc(
+									 * requestedPedidoId, empresa.getId(), 2) .stream()
+									 * .map(ordenCompraMapper::toDTO) .collect(Collectors.toList());
+									 * 
+									 * return !ordenCompraDTOs.isEmpty() ? ResponseEntity.ok(ordenCompraDTOs) :
+									 * ResponseEntity.noContent().build(); }
+									 * 
+									 * @PutMapping("/{requestedId}") private ResponseEntity<Void> putOrdenCompra(
+									 * 
+									 * @PathVariable Long requestedId, @RequestBody OrdenCompraDTO
+									 * ordenCompraDTOUpdate) { User authenticatedUser = getAuthenticatedUser();
+									 * Empresa empresa = getEmpresaFromUser(authenticatedUser); if
+									 * (ordenCompraRepository.existsByIdAndPedidoAlmacenSedeEmpresaId( requestedId,
+									 * empresa.getId()) &&
+									 * pedidoRepository.existsByIdAndAlmacenSedeEmpresaIdAndEstadoIdNot(
+									 * ordenCompraDTOUpdate.getPedido(), empresa.getId(), 2) &&
+									 * proveedorRepository.existsByIdAndEmpresaIdAndEstadoIdNot(
+									 * ordenCompraDTOUpdate.getProveedor(), empresa.getId(), 2)) { OrdenCompraDTO
+									 * updateOrdenCompraDTO = new OrdenCompraDTO( requestedId,
+									 * ordenCompraDTOUpdate.getFechaHora(), ordenCompraDTOUpdate.getPedido(),
+									 * ordenCompraDTOUpdate.getProveedor(), ordenCompraDTOUpdate.getDescripcion(),
+									 * ordenCompraDTOUpdate.getEstado()); OrdenCompra updatedOrdenCompra =
+									 * ordenCompraMapper.toEntity(updateOrdenCompraDTO);
+									 * ordenCompraRepository.save(updatedOrdenCompra); return
+									 * ResponseEntity.noContent().build(); } return
+									 * ResponseEntity.notFound().build(); }
+									 * 
+									 * @DeleteMapping("/{id}") private ResponseEntity<Void>
+									 * deleteOrdenCompra(@PathVariable Long id) { try { User authenticatedUser =
+									 * getAuthenticatedUser(); Empresa empresa =
+									 * getEmpresaFromUser(authenticatedUser); if
+									 * (ordenCompraRepository.existsByIdAndPedidoAlmacenSedeEmpresaId(id,
+									 * empresa.getId())) { ordenCompraRepository.deleteById(id); return
+									 * ResponseEntity.noContent().build(); } return
+									 * ResponseEntity.notFound().build(); } catch (Exception e) { return
+									 * ResponseEntity.internalServerError().build(); } }
+									 */
 
 }
