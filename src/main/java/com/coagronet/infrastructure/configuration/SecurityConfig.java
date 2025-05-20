f21d02254e9d2e58359aff7c90847796c3f8499b
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-20 11:21:25 -0500

    refactor: add almacen endpoint to security filter chain for role-based access control

--- src/main/java/com/coagronet/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/com/coagronet/infrastructure/configuration/SecurityConfig.java
@@ -48,7 +48,8 @@ public class SecurityConfig {
 						"/api/v1/grupo/**",						
 						"/api/v1/sede/**",
 						"/api/v1/bloque/**",
-						"/api/v1/espacio/**")
+						"/api/v1/espacio/**",
+						"/api/v1/almacen/**")
 				.hasAnyRole("ADMINISTRADOR_SISTEMA", "ADMINISTRADOR_EMPRESA")
 				.requestMatchers("/api/v2/report/**")
 				.hasAnyRole("ADMINISTRADOR_SISTEMA", "ADMINISTRADOR_EMPRESA", "GERENTE")
