5aa2bcc457e6e9976368e533e255be7530ff4cff
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-20 11:21:50 -0500

    refactor: update AlmacenDTO fields and validation annotations for improved data integrity

--- src/main/java/com/coagronet/almacen/dtos/AlmacenDTO.java
+++ src/main/java/com/coagronet/almacen/dtos/AlmacenDTO.java
@@ -1,5 +1,10 @@
 package com.coagronet.almacen.dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,14 +12,33 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AlmacenDTO {
-    private Integer id;
-    private String nombre;
-    private Long sedeId;
-    private String descripcion;
-    private Integer estadoId;
-    private String geolocalizacion;
-    private String coordenadas;
-    private Integer espacioId;
-    private String direccion;
+
+	private Long id;
+
+	@NotBlank(message = "El nombre es obligatorio.")
+	@Size(max = 100, message = "El nombre no debe superar los 100 caracteres.")
+	private String nombre;
+
+	@Size(max = 255, message = "La descripcion no debe superar los 255 caracteres.")
+	private String descripcion;
+
+	@NotNull(message = "El estado es obligatorio.")
+	private Long estadoId;
+
+	@Size(max = 255, message = "La geolocalizacion no debe superar los 255 caracteres.")
+	private String geolocalizacion;
+
+	@Size(max = 4096, message = "Las coordenadas no debe superar los 4096 caracteres.")
+	private String coordenadas;
+
+	@NotNull(message = "El espacio es obligatorio.")
+	private Long espacioId;
+
+	@Size(max = 255, message = "La direccion no debe superar los 255 caracteres.")
+	private String direccion;
+
+	private Long empresaId;
+
 }
