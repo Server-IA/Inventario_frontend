d831dc815f514b8e9bb3e80049c3f8807aaacfca
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-22 01:38:06 -0500

    feat: add ForgotPasswordRequestDTO for password recovery functionality

---
+++ src/main/java/com/coagronet/user/dtos/ForgotPasswordRequestDTO.java
@@ -0,0 +1,12 @@
+package com.coagronet.user.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ForgotPasswordRequestDTO {
+	@NotBlank(message = "El email es obligatorio")
+	@Email(message = "Formato de email inválido")
+	private String email;
+}
