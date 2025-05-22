23d21205e833089fb046d75d2fd8d3cfdeb14eba
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-05-22 01:37:57 -0500

    feat: add ResetPasswordRequestDTO for password reset functionality

---
+++ src/main/java/com/coagronet/user/dtos/ResetPasswordRequestDTO.java
@@ -0,0 +1,13 @@
+package com.coagronet.user.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ResetPasswordRequestDTO {
+	@NotBlank(message = "El token es obligatorio")
+	private String token;
+
+	@NotBlank(message = "La nueva contraseña es obligatoria")
+	private String newPassword;
+}
