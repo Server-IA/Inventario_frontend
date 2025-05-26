d82de19ce35a73a2a2c8fd5d5fd7c545b6c41c08
jujcgu <juanjosecastrogutierrez@gmail.com>
2025-05-26 17:41:37 -0500

    feat: create ArticuloKardexDTO for managing Kardex item data transfer

---
+++ src/main/java/com/coagronet/articuloKardex/dtos/ArticuloKardexDTO.java
@@ -0,0 +1,37 @@
+package com.coagronet.articuloKardex.dtos;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ArticuloKardexDTO {
+
+	private Long id;
+
+	private Double cantidad;
+
+	private Double precio;
+
+	private LocalDateTime fechaVencimiento;
+
+	@NotNull(message = "El kardex es obligatorio.")
+	private Long kardexId;
+
+	@NotNull(message = "La presentación de producto es obligatoria.")
+	private Long productoPresentacionId;
+
+	@NotNull(message = "El estado es obligatorio.")
+	private Long estadoId;
+
+	private Long empresaId;
+
+}
