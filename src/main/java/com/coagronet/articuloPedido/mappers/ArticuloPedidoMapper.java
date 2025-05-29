2cfbd9a4dbdf50b18a7364fdeefe1e34980452cb
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-05-29 18:50:09 -0500

    feat: add ArticuloPedidoMapper interface for mapping between ArticuloPedido and ArticuloPedidoDTO

---
+++ src/main/java/com/coagronet/articuloPedido/mappers/ArticuloPedidoMapper.java
@@ -0,0 +1,30 @@
+package com.coagronet.articuloPedido.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.coagronet.articuloPedido.ArticuloPedido;
+import com.coagronet.articuloPedido.dtos.ArticuloPedidoDTO;
+
+@Mapper(componentModel = "spring")
+public interface ArticuloPedidoMapper {
+	
+	@Mapping(source = "pedido.id", target = "pedidoId")
+	@Mapping(source = "productoPresentacion.id", target = "productoPresentacionId")
+	@Mapping(source = "estado.id", target = "estadoId")
+	@Mapping(source = "empresa.id", target = "empresaId")
+	ArticuloPedidoDTO toDTO(ArticuloPedido articuloPedido);
+
+	@Mapping(source = "pedidoId", target = "pedido.id")
+	@Mapping(source = "productoPresentacionId", target = "productoPresentacion.id")
+	@Mapping(source = "estadoId", target = "estado.id")
+	@Mapping(source = "empresaId", target = "empresa.id")
+	ArticuloPedido toEntity(ArticuloPedidoDTO articuloPedidoDTO);
+
+	@Mapping(source = "pedido.id", target = "pedidoId")
+	@Mapping(source = "productoPresentacion.id", target = "productoPresentacionId")
+	@Mapping(source = "estado.id", target = "estadoId")
+	@Mapping(source = "empresa.id", target = "empresaId", ignore = true)
+	ArticuloPedidoDTO toListDTO(ArticuloPedido articuloPedido);
+
+}
