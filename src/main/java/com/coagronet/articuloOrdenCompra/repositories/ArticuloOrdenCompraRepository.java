c30a3b2b97330de364faafa3844b17b4ab42da80
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-05-31 17:14:48 -0500

    Add: Create ArticuloOrdenCompraRepository interface for data access

---
+++ src/main/java/com/coagronet/articuloOrdenCompra/repositories/ArticuloOrdenCompraRepository.java
@@ -0,0 +1,16 @@
+package com.coagronet.articuloOrdenCompra.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.coagronet.articuloOrdenCompra.ArticuloOrdenCompra;
+
+public interface ArticuloOrdenCompraRepository extends JpaRepository<ArticuloOrdenCompra, Long> {
+
+    Optional<ArticuloOrdenCompra> findByIdAndEmpresaId(Long id, Long empresaId);
+
+    List<ArticuloOrdenCompra> findByEmpresaIdOrderByIdAsc(Long empresaId);
+
+}
