ecc8fffdc85b5c45f9056666ebe93363b4064986
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-06-12 13:20:51 -0500

    Add ArticuloKardexControllerV2 with endpoint to retrieve articles by kardexId

---
+++ src/main/java/com/coagronet/articuloKardex/controllers/ArticuloKardexControllerV2.java
@@ -0,0 +1,25 @@
+package com.coagronet.articuloKardex.controllers;
+
+import com.coagronet.articuloKardex.dtos.ArticuloKardexDTO;
+import com.coagronet.articuloKardex.services.ArticuloKardexService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class ArticuloKardexControllerV2 {
+
+    private final ArticuloKardexService articuloKardexService;
+
+    @GetMapping("/kardex/{kardexId}/articulos")
+    public ResponseEntity<List<ArticuloKardexDTO>> findArticulosByKardex(
+            @PathVariable Long kardexId) {
+        return ResponseEntity.ok(articuloKardexService.findAllByKardexId(kardexId));
+    }
+
+}
