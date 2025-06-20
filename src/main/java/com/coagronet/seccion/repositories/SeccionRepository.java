7644cbd5bfa5eebbaa1d5ed9a649f9b2bebb47d5
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-06-20 15:32:37 -0500

    Add SeccionRepository interface for database operations on Seccion entity

---
+++ src/main/java/com/coagronet/seccion/repositories/SeccionRepository.java
@@ -0,0 +1,18 @@
+package com.coagronet.seccion.repositories;
+
+import com.coagronet.proceso.Proceso;
+import com.coagronet.seccion.Seccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SeccionRepository extends JpaRepository<Seccion, Long> {
+
+    Optional<Seccion> findByIdAndEmpresaId(Long id, Long empresaId);
+
+    List<Seccion> findByEmpresaIdOrderByIdAsc(Long empresaId);
+
+}
