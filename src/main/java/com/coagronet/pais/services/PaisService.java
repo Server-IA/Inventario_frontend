e5f236b95353c300a174c9a96b53fefc6e88e39c
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-06-25 21:15:57 -0500

    Refactor PaisService to remove unused AuthenticationService and streamline empresaId retrieval

--- src/main/java/com/coagronet/pais/services/PaisService.java
+++ src/main/java/com/coagronet/pais/services/PaisService.java
@@ -6,13 +6,10 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
-import com.coagronet.empresa.Empresa;
 import com.coagronet.estado.repositories.EstadoRepository;
 import com.coagronet.pais.dtos.PaisDTO;
 import com.coagronet.pais.mappers.PaisMapper;
 import com.coagronet.pais.repositories.PaisRepository;
-import com.coagronet.user.User;
-import com.coagronet.utils.AuthenticationService;
 import com.coagronet.exceptionHandler.BadRequestException;
 import com.coagronet.exceptionHandler.NotFoundException;
 import com.coagronet.utils.UserEmpresaService;
@@ -25,67 +22,45 @@ public class PaisService {
 
 	private final PaisMapper paisMapper;
 	private final PaisRepository paisRepository;
-	private final AuthenticationService authenticationService;
 	private final UserEmpresaService userEmpresaService;
 	private final EstadoRepository estadoRepository;
 
 	public List<PaisDTO> findAll() {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		return paisRepository.findByEmpresaIdOrderByIdAsc(empresa.getId()).stream().map(paisMapper::toListDto)
+		return paisRepository.findByEmpresaIdOrderByIdAsc(userEmpresaService.getEmpresaIdFromCurrentRequest()).stream()
+				.map(paisMapper::toListDto)
 				.collect(Collectors.toList());
 	}
 
-	public List<PaisDTO> findAllAvailable() {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		return paisRepository.findByEmpresaIdAndEstadoIdNotOrderByIdAsc(empresa.getId(), 2L).stream()
-				.map(paisMapper::toListDto).collect(Collectors.toList());
-	}
-
 	public Optional<PaisDTO> findById(Long requestedId) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		return paisRepository.findByIdAndEmpresaId(requestedId, empresa.getId()).map(paisMapper::toListDto);
+		return paisRepository.findByIdAndEmpresaId(requestedId, userEmpresaService.getEmpresaIdFromCurrentRequest())
+				.map(paisMapper::toListDto);
 	}
 
 	public PaisDTO create(PaisDTO paisDTO) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
 		estadoRepository.findById(paisDTO.getEstadoId())
 				.orElseThrow(() -> new BadRequestException("El estado no es válido"));
 
 		paisDTO.setId(null);
-		paisDTO.setEmpresaId(empresa.getId());
+		paisDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
 
 		return paisMapper.toDTO(paisRepository.save(paisMapper.toEntity(paisDTO)));
 	}
 
 	public void update(Long requestedId, PaisDTO paisDTO) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		paisRepository.findByIdAndEmpresaId(requestedId, empresa.getId())
+		paisRepository.findByIdAndEmpresaId(requestedId, userEmpresaService.getEmpresaIdFromCurrentRequest())
 				.orElseThrow(() -> new NotFoundException("Pais no encontrado"));
 
 		estadoRepository.findById(paisDTO.getEstadoId())
 				.orElseThrow(() -> new BadRequestException("El estado no es válido"));
 
 		paisDTO.setId(requestedId);
-		paisDTO.setEmpresaId(empresa.getId());
+		paisDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
 
 		paisRepository.save(paisMapper.toEntity(paisDTO));
 	}
 
 	public void delete(Long id) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		paisRepository.findByIdAndEmpresaId(id, empresa.getId())
+		paisRepository.findByIdAndEmpresaId(id, userEmpresaService.getEmpresaIdFromCurrentRequest())
 				.orElseThrow(() -> new NotFoundException("Pais no encontrado"));
 
 		paisRepository.deleteById(id);
