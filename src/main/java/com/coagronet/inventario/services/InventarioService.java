cdbaed9cad63608b1fc8cc3d8dcd235319687b0d
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-06-25 21:13:22 -0500

    Refactor InventarioService to remove authentication logic and streamline empresaId retrieval

--- src/main/java/com/coagronet/inventario/services/InventarioService.java
+++ src/main/java/com/coagronet/inventario/services/InventarioService.java
@@ -1,15 +1,11 @@
 package com.coagronet.inventario.services;
 
-import com.coagronet.empresa.Empresa;
 import com.coagronet.estado.repositories.EstadoRepository;
 import com.coagronet.exceptionHandler.BadRequestException;
 import com.coagronet.exceptionHandler.NotFoundException;
 import com.coagronet.inventario.mappers.InventarioMapper;
 import com.coagronet.inventario.repositories.InventarioRepository;
-import com.coagronet.inventario.Inventario;
 import com.coagronet.inventario.dtos.InventarioDTO;
-import com.coagronet.user.User;
-import com.coagronet.utils.AuthenticationService;
 import com.coagronet.utils.UserEmpresaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,77 +22,51 @@ public class InventarioService {
     private final InventarioRepository inventarioRepository;
     private final EstadoRepository estadoRepository;
     private final InventarioMapper inventarioMapper;
-    private final AuthenticationService authenticationService;
     private final UserEmpresaService userEmpresaService;
 
-
-
-    public List<InventarioDTO> findAll(){
-        User authenticatedUser = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(authenticatedUser);
-        Long empresaId = empresa.getId();
-
-        return inventarioRepository.findByEmpresaIdOrderByIdAsc(empresaId)
+    public List<InventarioDTO> findAll() {
+        return inventarioRepository.findByEmpresaIdOrderByIdAsc(userEmpresaService.getEmpresaIdFromCurrentRequest())
                 .stream()
                 .map(inventarioMapper::toDTO)
                 .collect(Collectors.toList());
     }
 
-    public Optional<InventarioDTO> findById(Long requestedId){
-        User authenticatedUser = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(authenticatedUser);
-        Long empresaId = empresa.getId();
-
-        return inventarioRepository.findByIdAndEmpresaId(requestedId, empresaId)
+    public Optional<InventarioDTO> findById(Long requestedId) {
+        return inventarioRepository
+                .findByIdAndEmpresaId(requestedId, userEmpresaService.getEmpresaIdFromCurrentRequest())
                 .map(inventarioMapper::toDTO);
     }
 
-
     @Transactional
-    public InventarioDTO create(InventarioDTO inventarioDTO){
-        User authenticatedUser = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(authenticatedUser);
-        Long empresaId = empresa.getId();
-
+    public InventarioDTO create(InventarioDTO inventarioDTO) {
         estadoRepository.findById(inventarioDTO.getEstadoId())
-                .orElseThrow(()-> new BadRequestException("Estado no encontrado o no válido"));
+                .orElseThrow(() -> new BadRequestException("Estado no encontrado o no válido"));
 
-        inventarioDTO.setEmpresaId(empresaId);
-        Inventario inventario = inventarioMapper.toEntity(inventarioDTO);
-        inventario = inventarioRepository.save(inventario);
-        return inventarioMapper.toDTO(inventario);
+        inventarioDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
+
+        return inventarioMapper.toDTO(inventarioRepository.save(inventarioMapper.toEntity(inventarioDTO)));
     }
 
     @Transactional
-    public void update(Long requestedId, InventarioDTO inventarioDTO){
-        User authenticatedUser = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(authenticatedUser);
-        Long empresaId = empresa.getId();
-
-        inventarioRepository.findByIdAndEmpresaId(requestedId, empresaId)
-                .orElseThrow(()-> new NotFoundException("Inventario no encontrada en su empresa"));
+    public void update(Long requestedId, InventarioDTO inventarioDTO) {
+        inventarioRepository.findByIdAndEmpresaId(requestedId, userEmpresaService.getEmpresaIdFromCurrentRequest())
+                .orElseThrow(() -> new NotFoundException("Inventario no encontrada en su empresa"));
 
         estadoRepository.findById(inventarioDTO.getEstadoId())
-                .orElseThrow(()-> new BadRequestException("El estado no es válido"));
+                .orElseThrow(() -> new BadRequestException("El estado no es válido"));
 
         inventarioDTO.setId(requestedId);
-        inventarioDTO.setEmpresaId(empresaId);
+        inventarioDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
+
         inventarioRepository.save(inventarioMapper.toEntity(inventarioDTO));
     }
 
-
     @Transactional
-    public void delete(Long requestedId){
-        User authenticatedUser = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(authenticatedUser);
-        Long empresaId = empresa.getId();
-
-        inventarioRepository.findByIdAndEmpresaId(requestedId, empresaId)
-                .orElseThrow(()-> new NotFoundException("Inventario no encontrada en su empresa"));
+    public void delete(Long requestedId) {
+        inventarioRepository.findByIdAndEmpresaId(requestedId, userEmpresaService.getEmpresaIdFromCurrentRequest())
+                .orElseThrow(() -> new NotFoundException("Inventario no encontrada en su empresa"));
 
         inventarioRepository.deleteById(requestedId);
     }
-
-
 
 }
