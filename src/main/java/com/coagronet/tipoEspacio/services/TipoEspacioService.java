8bd54156bcca118de4a447b88809647ee6377313
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-06-25 21:17:11 -0500

    Refactor TipoEspacioService to remove unused AuthenticationService and streamline empresaId retrieval

--- src/main/java/com/coagronet/tipoEspacio/services/TipoEspacioService.java
+++ src/main/java/com/coagronet/tipoEspacio/services/TipoEspacioService.java
@@ -6,14 +6,11 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
-import com.coagronet.empresa.Empresa;
 import com.coagronet.estado.repositories.EstadoRepository;
 import com.coagronet.exceptionHandler.NotFoundException;
 import com.coagronet.tipoEspacio.dtos.TipoEspacioDTO;
 import com.coagronet.tipoEspacio.mappers.TipoEspacioMapper;
 import com.coagronet.tipoEspacio.repositories.TipoEspacioRepository;
-import com.coagronet.user.User;
-import com.coagronet.utils.AuthenticationService;
 import com.coagronet.utils.UserEmpresaService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,67 +22,52 @@ public class TipoEspacioService {
 	private final TipoEspacioRepository tipoEspacioRepository;
 	private final TipoEspacioMapper tipoEspacioMapper;
 	private final EstadoRepository estadoRepository;
-	private final AuthenticationService authenticationService;
 	private final UserEmpresaService userEmpresaService;
 
 	public List<TipoEspacioDTO> findAll() {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		return tipoEspacioRepository.findByEmpresaIdOrderByIdAsc(empresa.getId()).stream()
+		return tipoEspacioRepository.findByEmpresaIdOrderByIdAsc(userEmpresaService.getEmpresaIdFromCurrentRequest())
+				.stream()
 				.map(tipoEspacioMapper::toListDto).collect(Collectors.toList());
 	}
 
 	public List<TipoEspacioDTO> findAllAvailable() {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		return tipoEspacioRepository.findByEmpresaIdAndEstadoIdNotOrderByIdAsc(empresa.getId(), 2L).stream()
+		return tipoEspacioRepository
+				.findByEmpresaIdAndEstadoIdNotOrderByIdAsc(userEmpresaService.getEmpresaIdFromCurrentRequest(), 2L)
+				.stream()
 				.map(tipoEspacioMapper::toListDto).collect(Collectors.toList());
 	}
 
 	public Optional<TipoEspacioDTO> findById(Long requestedId) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		return tipoEspacioRepository.findByIdAndEmpresaId(requestedId, empresa.getId())
+		return tipoEspacioRepository
+				.findByIdAndEmpresaId(requestedId, userEmpresaService.getEmpresaIdFromCurrentRequest())
 				.map(tipoEspacioMapper::toListDto);
 	}
 
 	public TipoEspacioDTO create(TipoEspacioDTO tipoEspacioDTO) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
 		estadoRepository.findById(tipoEspacioDTO.getEstadoId())
 				.orElseThrow(() -> new NotFoundException("Estado no encontrado"));
 
 		tipoEspacioDTO.setId(null);
-		tipoEspacioDTO.setEmpresaId(empresa.getId());
+		tipoEspacioDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
 
 		return tipoEspacioMapper.toDTO(tipoEspacioRepository.save(tipoEspacioMapper.toEntity(tipoEspacioDTO)));
 	}
 
 	public void update(Long requestedId, TipoEspacioDTO tipoEspacioDTO) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		tipoEspacioRepository.findByIdAndEmpresaId(requestedId, empresa.getId())
+		tipoEspacioRepository.findByIdAndEmpresaId(requestedId, userEmpresaService.getEmpresaIdFromCurrentRequest())
 				.orElseThrow(() -> new NotFoundException("Tipo de espacio no encontrado"));
 
 		estadoRepository.findById(tipoEspacioDTO.getEstadoId())
 				.orElseThrow(() -> new NotFoundException("Estado no encontrado"));
 
 		tipoEspacioDTO.setId(requestedId);
-		tipoEspacioDTO.setEmpresaId(empresa.getId());
+		tipoEspacioDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
 
 		tipoEspacioRepository.save(tipoEspacioMapper.toEntity(tipoEspacioDTO));
 	}
 
 	public void delete(Long id) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		tipoEspacioRepository.findByIdAndEmpresaId(id, empresa.getId())
+		tipoEspacioRepository.findByIdAndEmpresaId(id, userEmpresaService.getEmpresaIdFromCurrentRequest())
 				.orElseThrow(() -> new NotFoundException("Tipo de espacio no encontrado"));
 
 		tipoEspacioRepository.deleteById(id);
