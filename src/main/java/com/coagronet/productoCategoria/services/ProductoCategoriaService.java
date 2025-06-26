c5554f07f63082f2977192cb4860a8400cea726a
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-06-25 21:16:30 -0500

    Refactor ProductoCategoriaService to remove unused AuthenticationService and streamline empresaId retrieval

--- src/main/java/com/coagronet/productoCategoria/services/ProductoCategoriaService.java
+++ src/main/java/com/coagronet/productoCategoria/services/ProductoCategoriaService.java
@@ -5,15 +5,12 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
-import com.coagronet.empresa.Empresa;
 import com.coagronet.estado.repositories.EstadoRepository;
 import com.coagronet.exceptionHandler.BadRequestException;
 import com.coagronet.exceptionHandler.NotFoundException;
 import com.coagronet.productoCategoria.dtos.ProductoCategoriaDTO;
 import com.coagronet.productoCategoria.mappers.ProductoCategoriaMapper;
 import com.coagronet.productoCategoria.repositories.ProductoCategoriaRepository;
-import com.coagronet.user.User;
-import com.coagronet.utils.AuthenticationService;
 import com.coagronet.utils.UserEmpresaService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,53 +21,42 @@ public class ProductoCategoriaService {
 
 	private final ProductoCategoriaMapper productoCategoriaMapper;
 	private final ProductoCategoriaRepository productoCategoriaRepository;
-	private final AuthenticationService authenticationService;
 	private final UserEmpresaService userEmpresaService;
 	private final EstadoRepository estadoRepository;
 
 	public List<ProductoCategoriaDTO> findAll() {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		return productoCategoriaRepository.findByEmpresaIdOrderByIdAsc(empresa.getId()).stream()
+		return productoCategoriaRepository
+				.findByEmpresaIdOrderByIdAsc(userEmpresaService.getEmpresaIdFromCurrentRequest()).stream()
 				.map(productoCategoriaMapper::toListDto).collect(Collectors.toList());
 	}
 
 	public ProductoCategoriaDTO create(ProductoCategoriaDTO productoCategoriaDTO) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
 		estadoRepository.findById(productoCategoriaDTO.getEstadoId())
 				.orElseThrow(() -> new BadRequestException("El estado no es válido"));
 
 		productoCategoriaDTO.setId(null);
-		productoCategoriaDTO.setEmpresaId(empresa.getId());
+		productoCategoriaDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
 
 		return productoCategoriaMapper
 				.toDTO(productoCategoriaRepository.save(productoCategoriaMapper.toEntity(productoCategoriaDTO)));
 	}
 
 	public void update(Long requestedId, ProductoCategoriaDTO productoCategoriaDTO) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		productoCategoriaRepository.findByIdAndEmpresaId(requestedId, empresa.getId())
+		productoCategoriaRepository
+				.findByIdAndEmpresaId(requestedId, userEmpresaService.getEmpresaIdFromCurrentRequest())
 				.orElseThrow(() -> new NotFoundException("Categoria de producto no encontrada"));
 
 		estadoRepository.findById(productoCategoriaDTO.getEstadoId())
 				.orElseThrow(() -> new BadRequestException("El estado no es válido"));
 
 		productoCategoriaDTO.setId(requestedId);
-		productoCategoriaDTO.setEmpresaId(empresa.getId());
+		productoCategoriaDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
 
 		productoCategoriaRepository.save(productoCategoriaMapper.toEntity(productoCategoriaDTO));
 	}
 
 	public void delete(Long id) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		productoCategoriaRepository.findByIdAndEmpresaId(id, empresa.getId())
+		productoCategoriaRepository.findByIdAndEmpresaId(id, userEmpresaService.getEmpresaIdFromCurrentRequest())
 				.orElseThrow(() -> new NotFoundException("Categoria de producto no encontrada"));
 
 		productoCategoriaRepository.deleteById(id);
