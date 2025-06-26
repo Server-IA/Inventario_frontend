0265f4bf83e2db7f73d25478d6cb9d608db52854
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-06-25 21:16:01 -0500

    Refactor PedidoService to remove unused AuthenticationService and streamline empresaId retrieval

--- src/main/java/com/coagronet/pedido/services/PedidoService.java
+++ src/main/java/com/coagronet/pedido/services/PedidoService.java
@@ -1,14 +1,10 @@
 package com.coagronet.pedido.services;
 
-import com.coagronet.empresa.Empresa;
 import com.coagronet.estado.repositories.EstadoRepository;
 import com.coagronet.exceptionHandler.NotFoundException;
-import com.coagronet.pedido.Pedido;
 import com.coagronet.pedido.dtos.PedidoDTO;
 import com.coagronet.pedido.mappers.PedidoMapper;
 import com.coagronet.pedido.repositories.PedidoRepository;
-import com.coagronet.user.User;
-import com.coagronet.utils.AuthenticationService;
 import com.coagronet.utils.UserEmpresaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,82 +18,55 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class PedidoService {
 
-
     private final PedidoRepository pedidoRepository;
     private final PedidoMapper pedidoMapper;
     private final UserEmpresaService userEmpresaService;
-    private final AuthenticationService authenticationService;
     private final EstadoRepository estadoRepository;
 
-
-
-    public List<PedidoDTO> findAll(){
-        User authenticatedUser = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(authenticatedUser);
-        Long empresaId = empresa.getId();
-
-        return pedidoRepository.findByEmpresaIdOrderByIdAsc(empresaId)
+    public List<PedidoDTO> findAll() {
+        return pedidoRepository.findByEmpresaIdOrderByIdAsc(userEmpresaService.getEmpresaIdFromCurrentRequest())
                 .stream()
                 .map(pedidoMapper::toDto)
                 .collect(Collectors.toList());
     }
 
-
-    public Optional<PedidoDTO> findById(Long requestId){
-        User authenticatedUser = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(authenticatedUser);
-        Long empresaId = empresa.getId();
-
-        return pedidoRepository.findByIdAndEmpresaId(requestId, empresaId)
+    public Optional<PedidoDTO> findById(Long requestId) {
+        return pedidoRepository.findByIdAndEmpresaId(requestId, userEmpresaService.getEmpresaIdFromCurrentRequest())
                 .map(pedidoMapper::toDto);
     }
 
-
     @Transactional
-    public PedidoDTO create(PedidoDTO pedidoDTO){
-        User authenticatedUser = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(authenticatedUser);
-        Long empresaId = empresa.getId();
-
+    public PedidoDTO create(PedidoDTO pedidoDTO) {
         estadoRepository.findById(pedidoDTO.getEstadoId())
-                .orElseThrow(()-> new NotFoundException("Estado no encontrado o no válido"));
+                .orElseThrow(() -> new NotFoundException("Estado no encontrado o no válido"));
 
-        pedidoDTO.setEmpresaId(empresaId);
-        Pedido pedido = pedidoMapper.toEntity(pedidoDTO);
-        pedido = pedidoRepository.save(pedido);
-        return pedidoMapper.toDto(pedido);
+        pedidoDTO.setId(null);
+        pedidoDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
+
+        return pedidoMapper.toDto(pedidoRepository.save(pedidoMapper.toEntity(pedidoDTO)));
     }
 
     @Transactional
-    public void update(Long requestId, PedidoDTO pedidoDTO){
-        User authenticatedUser = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(authenticatedUser);
-        Long empresaId = empresa.getId();
-
-        pedidoRepository.findByIdAndEmpresaId(requestId, empresaId)
-                .orElseThrow(()-> new NotFoundException("Pedido no encontrado o no válido"));
+    public void update(Long requestId, PedidoDTO pedidoDTO) {
+        pedidoRepository.findByIdAndEmpresaId(requestId, userEmpresaService.getEmpresaIdFromCurrentRequest())
+                .orElseThrow(() -> new NotFoundException("Pedido no encontrado o no válido"));
 
         estadoRepository.findById(pedidoDTO.getEstadoId())
-                .orElseThrow(()-> new NotFoundException("Estado no encontrado o no válido"));
-
+                .orElseThrow(() -> new NotFoundException("Estado no encontrado o no válido"));
 
         pedidoDTO.setId(requestId);
-        pedidoDTO.setEmpresaId(empresaId);
+        pedidoDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
+
         pedidoRepository.save(pedidoMapper.toEntity(pedidoDTO));
     }
 
     @Transactional
-    public void delete(Long requestId){
-        User authenticatedUser = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(authenticatedUser);
-        Long empresaId = empresa.getId();
-
-        pedidoRepository.findByIdAndEmpresaId(requestId, empresaId)
-                .orElseThrow(()-> new NotFoundException("Pedido no encontrado o no válido"));
+    public void delete(Long requestId) {
+        pedidoRepository.findByIdAndEmpresaId(requestId, userEmpresaService.getEmpresaIdFromCurrentRequest())
+                .orElseThrow(() -> new NotFoundException("Pedido no encontrado o no válido"));
 
         pedidoRepository.deleteById(requestId);
 
     }
-
 
 }
