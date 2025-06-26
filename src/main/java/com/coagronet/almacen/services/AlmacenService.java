1386fd30ee4ec9036b497c245f0fb78c9dcf6191
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-06-25 21:11:09 -0500

    Refactor AlmacenService to remove unused authentication logic and streamline methods

--- src/main/java/com/coagronet/almacen/services/AlmacenService.java
+++ src/main/java/com/coagronet/almacen/services/AlmacenService.java
@@ -9,13 +9,10 @@ import org.springframework.stereotype.Service;
 import com.coagronet.almacen.dtos.AlmacenDTO;
 import com.coagronet.almacen.mappers.AlmacenMapper;
 import com.coagronet.almacen.repositories.AlmacenRepository;
-import com.coagronet.empresa.Empresa;
 import com.coagronet.espacio.repositories.EspacioRepository;
 import com.coagronet.estado.repositories.EstadoRepository;
 import com.coagronet.exceptionHandler.BadRequestException;
 import com.coagronet.exceptionHandler.NotFoundException;
-import com.coagronet.user.User;
-import com.coagronet.utils.AuthenticationService;
 import com.coagronet.utils.UserEmpresaService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,74 +23,56 @@ public class AlmacenService {
 
 	private final AlmacenMapper almacenMapper;
 	private final AlmacenRepository almacenRepository;
-	private final AuthenticationService authenticationService;
 	private final UserEmpresaService userEmpresaService;
 	private final EspacioRepository espacioRepository;
 	private final EstadoRepository estadoRepository;
 
 	public List<AlmacenDTO> findAll() {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
 
-		return almacenRepository.findByEmpresaIdOrderByIdAsc(empresa.getId()).stream().map(almacenMapper::toListDto)
+		return almacenRepository.findByEmpresaIdOrderByIdAsc(userEmpresaService.getEmpresaIdFromCurrentRequest())
+				.stream().map(almacenMapper::toListDto)
 				.collect(Collectors.toList());
 	}
 
-	public List<AlmacenDTO> findAllAvailable() {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		return almacenRepository.findByEmpresaIdAndEstadoIdNotOrderByIdAsc(empresa.getId(), 2L).stream()
-				.map(almacenMapper::toListDto).collect(Collectors.toList());
-	}
-
 	public Optional<AlmacenDTO> findById(Long requestedId) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
 
-		return almacenRepository.findByIdAndEmpresaId(requestedId, empresa.getId()).map(almacenMapper::toListDto);
+		return almacenRepository.findByIdAndEmpresaId(requestedId, userEmpresaService.getEmpresaIdFromCurrentRequest())
+				.map(almacenMapper::toListDto);
 	}
 
 	public AlmacenDTO create(AlmacenDTO almacenDTO) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		espacioRepository.findByIdAndEmpresaId(almacenDTO.getEspacioId(), empresa.getId())
+		espacioRepository
+				.findByIdAndEmpresaId(almacenDTO.getEspacioId(), userEmpresaService.getEmpresaIdFromCurrentRequest())
 				.orElseThrow(() -> new BadRequestException("El espacio no es válido"));
 
 		estadoRepository.findById(almacenDTO.getEstadoId())
 				.orElseThrow(() -> new BadRequestException("El estado no es válido"));
 
 		almacenDTO.setId(null);
-		almacenDTO.setEmpresaId(empresa.getId());
+		almacenDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
 
 		return almacenMapper.toDTO(almacenRepository.save(almacenMapper.toEntity(almacenDTO)));
 	}
 
 	public void update(Long requestedId, AlmacenDTO almacenDTO) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		almacenRepository.findByIdAndEmpresaId(requestedId, empresa.getId())
+		almacenRepository.findByIdAndEmpresaId(requestedId, userEmpresaService.getEmpresaIdFromCurrentRequest())
 				.orElseThrow(() -> new NotFoundException("Almacen no encontrado"));
 
-		espacioRepository.findByIdAndEmpresaId(almacenDTO.getEspacioId(), empresa.getId())
+		espacioRepository
+				.findByIdAndEmpresaId(almacenDTO.getEspacioId(), userEmpresaService.getEmpresaIdFromCurrentRequest())
 				.orElseThrow(() -> new BadRequestException("El espacio no es válida"));
 
 		estadoRepository.findById(almacenDTO.getEstadoId())
 				.orElseThrow(() -> new BadRequestException("El estado no es válido"));
 
 		almacenDTO.setId(requestedId);
-		almacenDTO.setEmpresaId(empresa.getId());
+		almacenDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
 
 		almacenRepository.save(almacenMapper.toEntity(almacenDTO));
 	}
 
 	public void delete(Long id) {
-		User user = authenticationService.getAuthenticatedUser();
-		Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-
-		almacenRepository.findByIdAndEmpresaId(id, empresa.getId())
+		almacenRepository.findByIdAndEmpresaId(id, userEmpresaService.getEmpresaIdFromCurrentRequest())
 				.orElseThrow(() -> new NotFoundException("Almacen no encontrado"));
 
 		almacenRepository.deleteById(id);
