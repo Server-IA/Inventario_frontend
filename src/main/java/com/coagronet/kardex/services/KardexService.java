ed3a0395426b3a340b0510f8d09bd5758bb9f4b3
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-06-25 21:13:30 -0500

    Refactor KardexService to remove unused AuthenticationService and streamline empresaId retrieval

--- src/main/java/com/coagronet/kardex/services/KardexService.java
+++ src/main/java/com/coagronet/kardex/services/KardexService.java
@@ -1,15 +1,11 @@
 package com.coagronet.kardex.services;
 
-import com.coagronet.empresa.Empresa;
 import com.coagronet.estado.repositories.EstadoRepository;
 import com.coagronet.exceptionHandler.BadRequestException;
 import com.coagronet.exceptionHandler.NotFoundException;
 import com.coagronet.kardex.mappers.KardexMapper;
 import com.coagronet.kardex.repositories.KardexRepository;
-import com.coagronet.kardex.Kardex;
 import com.coagronet.kardex.dtos.KardexDTO;
-import com.coagronet.user.User;
-import com.coagronet.utils.AuthenticationService;
 import com.coagronet.utils.UserEmpresaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,69 +23,48 @@ public class KardexService {
     private final KardexMapper kardexMapper;
     private final EstadoRepository estadoRepository;
     private final UserEmpresaService userEmpresaService;
-    private final AuthenticationService authenticationService;
-
 
     public List<KardexDTO> findAll() {
-        User user = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-        Long empresaId = empresa.getId();
-        return kardexRepository.findByEmpresaIdOrderByIdAsc(empresaId)
+        return kardexRepository.findByEmpresaIdOrderByIdAsc(userEmpresaService.getEmpresaIdFromCurrentRequest())
                 .stream()
                 .map(kardexMapper::toDto)
                 .collect(Collectors.toList());
     }
 
     public Optional<KardexDTO> findById(Long requestedId) {
-        User user = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-        Long empresaId = empresa.getId();
-
-        return kardexRepository.findByIdAndEmpresaId(requestedId, empresaId)
+        return kardexRepository.findByIdAndEmpresaId(requestedId, userEmpresaService.getEmpresaIdFromCurrentRequest())
                 .map(kardexMapper::toDto);
     }
 
-
     @Transactional
     public KardexDTO create(KardexDTO kardexDTO) {
-        User user = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-        Long empresaId = empresa.getId();
-
         estadoRepository.findById(kardexDTO.getEstadoId())
-                .orElseThrow(()-> new BadRequestException("El estado no es válido"));
+                .orElseThrow(() -> new BadRequestException("El estado no es válido"));
 
-        kardexDTO.setEmpresaId(empresaId);
+        kardexDTO.setId(null);
+        kardexDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
 
-        Kardex kardex = kardexMapper.toEntity(kardexDTO);
-        kardex = kardexRepository.save(kardex);
-        return kardexMapper.toDto(kardex);
+        return kardexMapper.toDto(kardexRepository.save(kardexMapper.toEntity(kardexDTO)));
     }
 
     @Transactional
     public void update(Long requestedId, KardexDTO kardexDTO) {
-        User user = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-        Long empresaId = empresa.getId();
-
-        kardexRepository.findByIdAndEmpresaId(requestedId, empresaId)
-                .orElseThrow(()-> new NotFoundException("Kardex no encontrada o no válida"));
+        kardexRepository.findByIdAndEmpresaId(requestedId, userEmpresaService.getEmpresaIdFromCurrentRequest())
+                .orElseThrow(() -> new NotFoundException("Kardex no encontrada o no válida"));
 
         estadoRepository.findById(kardexDTO.getEstadoId())
-                .orElseThrow(()-> new BadRequestException("El estado no es válido"));
+                .orElseThrow(() -> new BadRequestException("El estado no es válido"));
 
         kardexDTO.setId(requestedId);
-        kardexDTO.setEmpresaId(empresaId);
+        kardexDTO.setEmpresaId(userEmpresaService.getEmpresaIdFromCurrentRequest());
+
         kardexRepository.save(kardexMapper.toEntity(kardexDTO));
     }
 
     @Transactional
     public void delete(Long requestId) {
-        User user = authenticationService.getAuthenticatedUser();
-        Empresa empresa = userEmpresaService.getEmpresaFromUser(user);
-        Long empresaId = empresa.getId();
-        kardexRepository.findByIdAndEmpresaId(requestId, empresaId)
-                .orElseThrow(()-> new NotFoundException("Kardex no encontrado o no válido"));
+        kardexRepository.findByIdAndEmpresaId(requestId, userEmpresaService.getEmpresaIdFromCurrentRequest())
+                .orElseThrow(() -> new NotFoundException("Kardex no encontrado o no válido"));
 
         kardexRepository.deleteById(requestId);
     }
