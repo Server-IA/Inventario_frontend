af39d1c988e46ffdbe09a433359631fb6c940774
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-07-06 13:17:50 -0500

    Update OrdenCompraMapper: add missing mapping methods for toListDTO and toRegister

--- src/main/java/com/coagronet/ordenCompra/mappers/OrdenCompraMapper.java
+++ src/main/java/com/coagronet/ordenCompra/mappers/OrdenCompraMapper.java
@@ -9,7 +9,6 @@ import com.coagronet.ordenCompra.dtos.OrdenCompraDTO;
 @Mapper(componentModel = "spring")
 public interface OrdenCompraMapper {
 
-
     @Mapping(source = "pedido.id", target = "pedidoId")
     @Mapping(source = "proveedor.id", target = "proveedorId")
     @Mapping(source = "estado.id", target = "estadoId")
@@ -21,5 +20,18 @@ public interface OrdenCompraMapper {
     @Mapping(source = "estadoId", target = "estado.id")
     @Mapping(source = "empresaId", target = "empresa.id")
     OrdenCompra toEntity(OrdenCompraDTO ordenCompraDTO);
+
+    @Mapping(source = "pedido.id", target = "pedidoId")
+    @Mapping(source = "proveedor.id", target = "proveedorId")
+    @Mapping(source = "estado.id", target = "estadoId")
+    @Mapping(source = "empresa.id", target = "empresaId", ignore = true)
+    OrdenCompraDTO toListDTO(OrdenCompra ordenCompra);
+
+    @Mapping(source = "id", target = "id", ignore = true)
+    @Mapping(source = "pedidoId", target = "pedido.id")
+    @Mapping(source = "proveedorId", target = "proveedor.id")
+    @Mapping(source = "estadoId", target = "estado.id")
+    @Mapping(source = "empresaId", target = "empresa.id", ignore = true)
+    OrdenCompra toRegister(OrdenCompraDTO ordenCompraDTO);
 
 }
