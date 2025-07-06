51980804f0655df1d735a20dc4b9fb6a5d9caa2f
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-07-06 16:08:22 -0500

    Update CriterioEvaluacionDTO: enhance validation annotations and adjust field names for clarity

--- src/main/java/com/coagronet/criterioEvaluacion/dtos/CriterioEvaluacionDTO.java
+++ src/main/java/com/coagronet/criterioEvaluacion/dtos/CriterioEvaluacionDTO.java
@@ -1,5 +1,10 @@
 package com.coagronet.criterioEvaluacion.dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,10 +12,23 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CriterioEvaluacionDTO {
+
     private Long id;
-    private Long tipoEvaluacion;
+
+    @Size(max = 100, message = "El nombre debe tener máximo 100 caracteres.")
+    @NotBlank(message = "El nombre no puede estar vacío.")
     private String nombre;
+
+    @Size(max = 2048, message = "La descripción debe tener máximo 2048 caracteres.")
     private String descripcion;
-    private Long estado;
+
+    @NotNull(message = "El ID del tipo de evaluación no puede ser nulo.")
+    private Long tipoEvaluacionId;
+
+    @NotNull(message = "El ID del estado no puede ser nulo.")
+    private Long estadoId;
+
+    private Long empresaId;
 }
