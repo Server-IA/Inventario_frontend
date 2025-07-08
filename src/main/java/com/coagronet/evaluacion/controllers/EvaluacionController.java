43c557fa62f1218f45bd917aee55ad4f23fa317e
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-07-08 14:51:20 -0500

    Add EvaluacionController: implement CRUD operations for Evaluacion entity

---
+++ src/main/java/com/coagronet/evaluacion/controllers/EvaluacionController.java
@@ -0,0 +1,66 @@
+package com.coagronet.evaluacion.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.coagronet.evaluacion.dtos.EvaluacionDTO;
+import com.coagronet.evaluacion.services.EvaluacionService;
+import com.coagronet.utils.UriBuilderUtil;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/evaluacion")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class EvaluacionController {
+
+    private final EvaluacionService evaluacionService;
+    private final UriBuilderUtil uriBuilderUtil;
+
+    @GetMapping
+    public ResponseEntity<List<EvaluacionDTO>> findAll() {
+        return ResponseEntity.ok(evaluacionService.findAll());
+    }
+
+    @GetMapping("/{requestedId}")
+    public ResponseEntity<EvaluacionDTO> findById(@PathVariable Long requestedId) {
+        return evaluacionService.findById(requestedId).map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createEvaluacion(@Valid @RequestBody EvaluacionDTO evaluacionDTO,
+            UriComponentsBuilder ucb) {
+        return ResponseEntity
+                .created(uriBuilderUtil
+                        .buildEvaluacionUri((evaluacionService.create(evaluacionDTO)).getId(), ucb))
+                .build();
+    }
+
+    @PutMapping("/{requestedId}")
+    public ResponseEntity<Void> updateEvaluacion(@PathVariable Long requestedId,
+            @Valid @RequestBody EvaluacionDTO evaluacionDTO) {
+        evaluacionService.update(requestedId, evaluacionDTO);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEvaluacion(@PathVariable Long id) {
+        evaluacionService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
