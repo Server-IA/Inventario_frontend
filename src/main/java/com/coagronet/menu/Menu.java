0c598b1b4d526a7c385cb6d7f6c505861d83c076
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2025-07-15 15:31:27 -0500

    refactor se modifico la logica de la obtencion de roles en el controlador, ahora pasa a depender de UserRoleService

--- src/main/java/com/coagronet/menu/Menu.java
+++ src/main/java/com/coagronet/menu/Menu.java
@@ -11,18 +11,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.coagronet.utils.UserRoleService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/v1/menu")
 @CrossOrigin(origins = "*")
+@RequiredArgsConstructor
 public class Menu {
+
+    private final UserRoleService userRoleService;
 
     @GetMapping
     public ResponseEntity<String> getJson(Authentication authentication) throws IOException {
-        // Obtener el rol del usuario autenticado
-        String role = authentication.getAuthorities().iterator().next().getAuthority();
 
         // Determinar el archivo JSON según el rol
-        String filename = switch (role) {
+        String filename = switch (userRoleService.getRoleFromCurrentRequest()) {
             case "ROLE_ADMINISTRADOR_SISTEMA" -> "menu_administrador_sistema.json";
             case "ROLE_ADMINISTRADOR_EMPRESA" -> "menu_administrador_empresa.json";
             case "ROLE_GERENTE" -> "menu_gerente.json";
