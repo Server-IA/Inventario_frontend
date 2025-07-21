585b84e4b3b7198756579985cb5e96f55b1af92d
Juan Castro <juanjosecastrogutierrez@gmail.com>
2025-07-21 00:11:53 -0500

    chore(config): enable transaction management and async processing

--- src/main/java/com/coagronet/infrastructure/configuration/AppConfig.java
+++ src/main/java/com/coagronet/infrastructure/configuration/AppConfig.java
@@ -5,10 +5,14 @@ import java.util.Map;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @PropertySource("classpath:queries.properties")
 @ConfigurationProperties(prefix = "myapp")
+@EnableTransactionManagement
+@EnableAsync
 public class AppConfig {
 
     private Map<String, String> queries;
