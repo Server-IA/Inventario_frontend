542cee93aa3e481b740654537e5f0686a90ec142
David Oliveros <61753685+ZenwayProjects@users.noreply.github.com>
2025-08-10 23:41:01 -0500

    Feature/paginacion 10 (#91)
    * fix: se añade paginacion a los controladores marca y producto
    
    * fix: se añade paginacion de criterio evaluacion
    
    * fix: se añade paginacion de espacio
    
    * fix: se añade paginacion de EspacioOcupacion
    
    * fix: se añade paginacion de Evaluacion
    
    * fix: se añade paginacion de Inventario.
    
    * fix: se añade paginacion de InventarioItem.
    
    * fix: se añade paginacion de Ingrediente
    
    * fix: se añade paginacion de Kardex y KardexItem
    
    * fix: se añade paginacion de Kardex y KardexItem

--- src/main/java/com/coagronet/marca/repositories/MarcaRepository.java
+++ src/main/java/com/coagronet/marca/repositories/MarcaRepository.java
@@ -1,8 +1,9 @@
 package com.coagronet.marca.repositories;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,10 +12,8 @@ import com.coagronet.marca.Marca;
 @Repository
 public interface MarcaRepository extends JpaRepository<Marca, Long> {
 
-	List<Marca> findByEmpresaIdOrderByIdAsc(Long empresaId);
+	Page<Marca> findByEmpresaIdOrderByIdAsc(Long empresaId, Pageable pageable);
 
 	Optional<Marca> findByIdAndEmpresaId(Long id, Long empresaId);
-
-	boolean existsByIdAndEmpresaIdAndEstadoIdNot(Long id, Long empresaId, Long estadoId);
 
 }
