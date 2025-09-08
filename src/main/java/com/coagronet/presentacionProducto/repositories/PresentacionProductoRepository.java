6143b40db494b41da3e22f980f4fd49bc796ea00
Zenway <zenway15@gmail.com>
2025-09-07 20:47:45 -0500

    feat: paginacion producto_presentacion

--- src/main/java/com/coagronet/presentacionProducto/repositories/PresentacionProductoRepository.java
+++ src/main/java/com/coagronet/presentacionProducto/repositories/PresentacionProductoRepository.java
@@ -1,17 +1,18 @@
 package com.coagronet.presentacionProducto.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.coagronet.presentacionProducto.PresentacionProducto;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PresentacionProductoRepository extends JpaRepository<PresentacionProducto, Long> {
 
-	List<PresentacionProducto> findByEmpresaIdOrderByIdAsc(Long empresaId);
+	Page<PresentacionProducto> findByEmpresaIdOrderByIdAsc(Long empresaId, Pageable pageable);
 
 	Optional<PresentacionProducto> findByIdAndEmpresaId(Long id, Long empresaId);
 
