4093bd727e7fff106b98ed049fd645cfb370f753
Juan Jose Castro <juanjosecastrogutierrez@gmail.com>
2025-12-15 08:16:17 -0500

    feat(security): update security configuration to permit access to change password endpoint

--- src/main/java/com/coagronet/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/com/coagronet/infrastructure/configuration/SecurityConfig.java
@@ -34,11 +34,12 @@ public class SecurityConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth
 				.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/auth/**",
-						"/api/v1/empresas/**", "/api/v1/personas/**", "/api/v1/menu/**")
+						"/api/v1/empresas/**", "/api/v1/personas/**", "/api/v1/menu/**", "/change-password-initial/**")
 				.permitAll()
 				.requestMatchers("/api/v1/tipo_identificacion/**", "/api/v1/estado/**", "/api/v1/persona/**",
 						"/api/v1/movimiento/**", "/api/v1/tipo-evaluacion/**", "/api/v1/user/**",
-						"/api/v1/categoria-estado/**", "/api/v1/roles/**", "/api/v1/system/usuario-roles/**", "/api/v1/system/empresa-rol/**")
+						"/api/v1/categoria-estado/**", "/api/v1/roles/**", "/api/v1/system/usuario-roles/**",
+						"/api/v1/system/empresa-rol/**")
 				.hasAnyRole("ADMINISTRADOR_SISTEMA")
 				// Especifidad para unidad y tipoUnidad
 				.requestMatchers(HttpMethod.GET, "/api/v1/tipo-unidad/**").authenticated()
@@ -64,7 +65,8 @@ public class SecurityConfig {
 						"/api/v1/producto_presentacion/**", "/api/v1/produccion/**", "/api/v2/producto/**",
 						"/api/v1/producto_localizacion/**", "/api/v1/subseccion/**", "/api/report/**",
 						"/api/v1/factura/**", "/api/v1/pedido-cotizacion/**", "/api/v2/menu", "/api/v1/metricas/**",
-						"/api/v1/estado_categoria/**", "/api/v1/empresa-rol/**", "/api/v1/usuario-roles/**")
+						"/api/v1/estado_categoria/**", "/api/v1/empresa-rol/**", "/api/v1/usuario-roles/**",
+						"/empresa/usuarios-roles/**")
 				.hasAnyRole("ADMINISTRADOR_SISTEMA", "ADMINISTRADOR_EMPRESA").requestMatchers("/api/v2/report/**")
 				.hasAnyRole("ADMINISTRADOR_SISTEMA", "ADMINISTRADOR_EMPRESA", "GERENTE").anyRequest().authenticated())
 				.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
