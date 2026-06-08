12305e2fec7307097ce494894efd408080efa16a
Juan Jose Castro Gutierrez <juanjosecastrogutierrez@gmail.com>
2026-06-08 04:05:20 -0500

    fix(ArticuloKardex): add support for standardized auditing metadata

--- src/main/java/com/coagronet/articuloKardex/mappers/ArticuloKardexMapper.java
+++ src/main/java/com/coagronet/articuloKardex/mappers/ArticuloKardexMapper.java
@@ -14,18 +14,22 @@ public interface ArticuloKardexMapper {
 	@Mapping(source = "presentacionProducto.id", target = "presentacionProductoId")
 	@Mapping(source = "estado.id", target = "estadoId")
 	@Mapping(source = "empresa.id", target = "empresaId")
+	@Mapping(source = "username.username", target = "username")
 	ArticuloKardexDTO toDTO(ArticuloKardex articuloKardex);
 
 	@Mapping(source = "kardexId", target = "kardex.id")
 	@Mapping(source = "presentacionProductoId", target = "presentacionProducto.id")
 	@Mapping(source = "estadoId", target = "estado.id")
 	@Mapping(source = "empresaId", target = "empresa.id")
+	@Mapping(target = "username", ignore = true)
+	@Mapping(target = "fechaHora", ignore = true)
 	ArticuloKardex toEntity(ArticuloKardexDTO articuloKardexDTO);
 
 	@Mapping(source = "kardex.id", target = "kardexId")
 	@Mapping(source = "presentacionProducto.id", target = "presentacionProductoId")
 	@Mapping(source = "estado.id", target = "estadoId")
 	@Mapping(source = "empresa.id", target = "empresaId", ignore = true)
+	@Mapping(source = "username.username", target = "username")
 	ArticuloKardexDTO toListDTO(ArticuloKardex articuloKardex);
 
 	@Mapping(target = "id", ignore = true)
@@ -34,6 +38,7 @@ public interface ArticuloKardexMapper {
 	@Mapping(target = "estado", ignore = true)
 	@Mapping(target = "empresa", ignore = true)
 	@Mapping(target = "fechaHora", ignore = true)
+	@Mapping(target = "username", ignore = true)
 	void updateEntityFromDto(ArticuloKardexDTO dto, @MappingTarget ArticuloKardex entity);
 
 }
